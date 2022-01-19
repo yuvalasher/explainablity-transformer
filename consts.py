954f54cf77ab4642f27d7a52258ddb3b9862e610
Yuval Asher <asheryuvala@gmail.com>
2022-01-19 15:59:44 +0200

    add consts

--- consts.py
+++ consts.py
@@ -2,5 +2,7 @@ from pathlib import Path
 from config import config
 
 DATA_PATH: Path = Path(Path.cwd(), 'data')
+PLOTS_PATH: Path = Path(Path.cwd(), 'research', 'plots')
 images_folder_path = Path(DATA_PATH, config['general']['images_folder_name'])
 images_labels_gt_path = Path(DATA_PATH, config['general']['images_gt_filename'])
+PICKLES_FOLDER_PATH: Path = Path(Path.cwd(), 'pickles').resolve()
