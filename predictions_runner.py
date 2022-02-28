b643096455d074f2a885c0dfbbf933e1bb0b98a0
Yuval Asher <asheryuvala@gmail.com>
2022-02-28 15:05:23 +0200

    prediction runner on pictures - get the class idx at topk classes

--- predictions_runner.py
+++ predictions_runner.py
@@ -6,19 +6,18 @@ from pytorch_lightning import seed_everything
 vit_config = config['vit']
 seed_everything(config['general']['seed'])
 feature_extractor, vit_model = load_feature_extractor_and_vit_model(vit_config=vit_config)
-
-for idx, image_name in enumerate(os.listdir(IMAGES_FOLDER_PATH)[30:]):
-    try:
+l = ['0000000003.JPEG', '0000000004.JPEG','0000000002.JPEG', '0000000001.JPEG']#, '00000082.JPEG', '00000032.JPEG', '00000052.JPEG', '00000069.JPEG']
+for idx, image_name in enumerate(os.listdir(IMAGES_FOLDER_PATH)):
+    if image_name in l:
         image = get_image_from_path(os.path.join(IMAGES_FOLDER_PATH, image_name))
         inputs = feature_extractor(images=image, return_tensors="pt")
         outputs = vit_model(**inputs)
         logits = outputs.logits
         predicted_class_idx = logits.argmax(-1).item()
-        correct_class_prob = F.softmax(logits[0])[predicted_class_idx].item()
+        correct_class_prob = F.softmax(logits[0], dim=-1)[predicted_class_idx].item()
         correct_class_logits = torch.max(logits[0])
-        if correct_class_prob <= 0.85 and correct_class_prob >= 0.75:
-            print(image_name, vit_model.config.id2label[predicted_class_idx], correct_class_prob, correct_class_logits)
-    except:
-        pass
-    if idx == 700:
-        break
+        print(image_name, vit_model.config.id2label[predicted_class_idx], correct_class_prob, correct_class_logits.item())
+        probs, indices = torch.topk(F.softmax(logits[0], dim=-1), k=10, largest=True)
+        for prob, ind in zip(probs, indices):
+            print(f'Class: {vit_model.config.id2label[ind.item()]}, Prob: {prob.item()}, Class Idx: {ind.item()}')
+        print('----------------------------------------------------------------------------------------------------------------')
