23ef6942c5f662ecdd5156f590bf74eec873d75f
Yuval Asher <asheryuvala@gmail.com>
2022-03-03 19:35:34 +0200

    W&B every image separately

--- log_utils.py
+++ log_utils.py
@@ -1,13 +1,25 @@
 import wandb
 from typing import Dict
 
-def configure_log(vit_config: Dict, experiment_name: str):
+def configure_log(vit_config: Dict, experiment_name: str, image_name: str):
     print(experiment_name)
     if vit_config['log']:
         wandb.config = {
             "learning_rate": vit_config['lr'],
-            "experiment_name": experiment_name
+            "experiment_name": experiment_name,
+            "image_name": image_name
         }
         run = wandb.init(project="vit-sigmoid-1", entity="yuvalasher", config=wandb.config)
         return run
+    return None
+
+
+def get_wandb_config(vit_config: Dict, experiment_name: str, image_name: str):
+    if vit_config['log']:
+        wandb.config = {
+            "learning_rate": vit_config['lr'],
+            "experiment_name": experiment_name,
+            "image_name": image_name
+        }
+        return wandb.config
     return None
