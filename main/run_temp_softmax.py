fb9fec7fd59b07e7eee19d9640527244dad1f149
Yuval Asher <asheryuvala@gmail.com>
2022-03-03 19:34:57 +0200

    Make log to W&B will be written to every image separately

--- main/run_temp_softmax.py
+++ main/run_temp_softmax.py
@@ -3,7 +3,8 @@ from tqdm import tqdm
 from torch import optim
 from utils import *
 from loss_utils import *
-from log_utils import configure_log
+import wandb
+from log_utils import configure_log, get_wandb_config
 from utils.consts import *
 from pytorch_lightning import seed_everything
 from utils.transformation import pil_to_resized_tensor_transform
@@ -45,12 +46,6 @@ def compare_results_each_n_steps(iteration_idx: int, target: Tensor, output: Ten
         f'Is predicted same class: {is_predicted_same_class}, Correct Class Prob: {F.softmax(output, dim=-1)[0][torch.argmax(F.softmax(target, dim=-1)).item()]}')
     if is_predicted_same_class is False:
         print(f'Predicted class change at {iteration_idx} iteration !!!!!!')
-    # if is_iteration_to_action(iteration_idx=iteration_idx, action='print'):
-    #     print('temp')
-    #     print(prev_x_attention)
-    #     if sampled_binary_patches is not None and vit_config['objective'] != 'objective_gumble_minimize_softmax':
-    #         print(sampled_binary_patches)
-    # print(prev_x_attention.grad)
 
 
 def compare_between_predicted_classes(vit_logits: Tensor, vit_s_logits: Tensor) -> Tuple[bool, float]:
@@ -89,83 +84,83 @@ def objective_temp_softmax(output: Tensor, target: Tensor, temp: Tensor) -> Tens
     return loss
 
 
-def optimize_params(vit_model: ViTForImageClassification, criterion: Callable, log_run):
+def optimize_params(vit_model: ViTForImageClassification, criterion: Callable):
     for idx, image_dict in enumerate(vit_config['images']):
         image_name, correct_class_idx, contrastive_class_idx = image_dict['image_name'], image_dict['correct_class'], \
                                                                image_dict['contrastive_class']
-        vit_sigmoid_model = handle_model_config_and_freezing_for_task(
-            model=load_ViTModel(vit_config, model_type='softmax_temp'),
-            freezing_transformer=vit_config['freezing_transformer'])
-        print_number_of_trainable_and_not_trainable_params(model=vit_sigmoid_model, model_name='soft_temp')
-        optimizer = optim.Adam([vit_sigmoid_model.vit.encoder.x_attention], lr=vit_config['lr'])
+        wandb_config = get_wandb_config(vit_config=vit_config, experiment_name=experiment_name, image_name=image_name)
+        with wandb.init(project="vit-temp-softmax", entity="yuvalasher", config=wandb_config) as run:
+            vit_sigmoid_model = handle_model_config_and_freezing_for_task(
+                model=load_ViTModel(vit_config, model_type='softmax_temp'),
+                freezing_transformer=vit_config['freezing_transformer'])
+            print_number_of_trainable_and_not_trainable_params(model=vit_sigmoid_model, model_name='soft_temp')
+            optimizer = optim.Adam([vit_sigmoid_model.vit.encoder.x_attention], lr=vit_config['lr'])
 
-        image_plot_folder_path = get_and_create_image_plot_folder_path(images_folder_path=IMAGES_FOLDER_PATH,
-                                                                       experiment_name=experiment_name,
-                                                                       image_name=image_name)
-        save_url_to_text_file(path=image_plot_folder_path, log_run=log_run) if log_run is not None else []
-        image = get_image_from_path(Path(IMAGES_FOLDER_PATH, image_name))
-        inputs = feature_extractor(images=image, return_tensors="pt")
-        original_transformed_image = pil_to_resized_tensor_transform(image)
-        target = vit_model(**inputs)
-        vit_basic_for_dino = handle_model_config_and_freezing_for_task(
-            model=load_ViTModel(vit_config, model_type='vit-for-dino'))
-        _ = vit_basic_for_dino(**inputs)  # run forward to save attention_probs
-        dino_method_attention_probs_cls_on_tokens_last_layer(vit_sigmoid_model=vit_basic_for_dino,
-                                                             path=image_plot_folder_path)
-        attention_probs = get_attention_probs(model=vit_basic_for_dino)
-        plot_attention_rollout(attention_probs=attention_probs, path=image_plot_folder_path,
-                               patch_size=vit_config['patch_size'], iteration_idx=0, head_fusion='max')
-        plot_attention_rollout(attention_probs=attention_probs, path=image_plot_folder_path,
-                               patch_size=vit_config['patch_size'], iteration_idx=0, head_fusion='mean')
-        total_losses = []
-        prediction_losses = []
-        tokens_mask = []
-        x_attention = []
+            image_plot_folder_path = get_and_create_image_plot_folder_path(images_folder_path=IMAGES_FOLDER_PATH,
+                                                                           experiment_name=experiment_name,
+                                                                           image_name=image_name)
+            save_url_to_text_file(path=image_plot_folder_path, log_run=run) if run is not None else []
+            image = get_image_from_path(Path(IMAGES_FOLDER_PATH, image_name))
+            inputs = feature_extractor(images=image, return_tensors="pt")
+            original_transformed_image = pil_to_resized_tensor_transform(image)
+            target = vit_model(**inputs)
+            vit_basic_for_dino = handle_model_config_and_freezing_for_task(
+                model=load_ViTModel(vit_config, model_type='vit-for-dino'))
+            _ = vit_basic_for_dino(**inputs)  # run forward to save attention_probs
+            dino_method_attention_probs_cls_on_tokens_last_layer(vit_sigmoid_model=vit_basic_for_dino,
+                                                                 path=image_plot_folder_path)
+            attention_probs = get_attention_probs(model=vit_basic_for_dino)
+            plot_attention_rollout(attention_probs=attention_probs, path=image_plot_folder_path,
+                                   patch_size=vit_config['patch_size'], iteration_idx=0, head_fusion='max')
+            plot_attention_rollout(attention_probs=attention_probs, path=image_plot_folder_path,
+                                   patch_size=vit_config['patch_size'], iteration_idx=0, head_fusion='mean')
+            total_losses = []
+            prediction_losses = []
+            tokens_mask = []
+            x_attention = []
+            for iteration_idx in tqdm(range(vit_config['num_steps'])):
+                optimizer.zero_grad()
+                output = vit_sigmoid_model(**inputs)
+                prediction_losses.append(ce_loss(output.logits, torch.argmax(target.logits).unsqueeze(0)) * loss_config[
+                    'pred_loss_multiplier'])
+                x_attention.append(vit_sigmoid_model.vit.encoder.x_attention.clone())
+                loss = criterion(output=output.logits, target=target.logits,
+                                 temp=vit_sigmoid_model.vit.encoder.x_attention)
+                loss.backward()
+                total_losses.append(loss.item())
+                cls_attentions_probs = get_attention_probs_by_head_of_the_CLS(model=vit_sigmoid_model)
+                tokens_mask.append(cls_attentions_probs.clone())
+                compare_results_each_n_steps(iteration_idx=iteration_idx, target=target.logits, output=output.logits,
+                                             prev_x_attention=vit_sigmoid_model.vit.encoder.x_attention,
+                                             sampled_binary_patches=None)
 
-        for iteration_idx in tqdm(range(vit_config['num_steps'])):
-            optimizer.zero_grad()
-            output = vit_sigmoid_model(**inputs)
-            prediction_losses.append(ce_loss(output.logits, torch.argmax(target.logits).unsqueeze(0)) * loss_config[
-                'pred_loss_multiplier'])
-            x_attention.append(vit_sigmoid_model.vit.encoder.x_attention.clone())
-            loss = criterion(output=output.logits, target=target.logits,
-                             temp=vit_sigmoid_model.vit.encoder.x_attention)
-            loss.backward()
-            total_losses.append(loss.item())
-            cls_attentions_probs = get_attention_probs_by_head_of_the_CLS(model=vit_sigmoid_model)
-            tokens_mask.append(cls_attentions_probs.clone())
-            compare_results_each_n_steps(iteration_idx=iteration_idx, target=target.logits, output=output.logits,
-                                         prev_x_attention=vit_sigmoid_model.vit.encoder.x_attention,
-                                         sampled_binary_patches=None)
+                # plot_attn_probs(attentions=cls_attentions_probs, image_size=vit_config['img_size'],
+                #                 patch_size=vit_config['patch_size'], path=image_plot_folder_path,
+                #                 iteration_idx=iteration_idx, num_heads=vit_config['n_heads'])
+                save_saliency_map(image=original_transformed_image,
+                                  saliency_map=torch.tensor(
+                                      get_scores(cls_attentions_probs.mean(dim=0))).unsqueeze(0),
+                                  filename=Path(image_plot_folder_path, f'plot_{iteration_idx}'),
+                                  verbose=False)
+                # verbose=is_iteration_to_action(iteration_idx=iteration_idx, action='print'))
 
-            # plot_attn_probs(attentions=cls_attentions_probs, image_size=vit_config['img_size'],
-            #                 patch_size=vit_config['patch_size'], path=image_plot_folder_path,
-            #                 iteration_idx=iteration_idx, num_heads=vit_config['n_heads'])
-            save_saliency_map(image=original_transformed_image,
-                              saliency_map=torch.tensor(
-                                  get_scores(cls_attentions_probs.mean(dim=0))).unsqueeze(0),
-                              filename=Path(image_plot_folder_path, f'plot_{iteration_idx}'),
-                              verbose=False)
-            # verbose=is_iteration_to_action(iteration_idx=iteration_idx, action='print'))
+                optimizer.step()
+                if is_iteration_to_action(iteration_idx=iteration_idx, action='save'):
+                    save_obj_to_disk(path=Path(image_plot_folder_path, 'losses'), obj=prediction_losses)
+                    save_obj_to_disk(path=Path(image_plot_folder_path, 'total_losses'), obj=total_losses)
+                    save_obj_to_disk(path=Path(image_plot_folder_path, 'tokens_mask'), obj=tokens_mask)
+                    # save_model(model=vit_sigmoid_model, path=Path(f'{image_plot_folder_path}', 'vit_sigmoid_model'))
 
-            optimizer.step()
-            if is_iteration_to_action(iteration_idx=iteration_idx, action='save'):
-                save_obj_to_disk(path=Path(image_plot_folder_path, 'losses'), obj=prediction_losses)
-                save_obj_to_disk(path=Path(image_plot_folder_path, 'total_losses'), obj=total_losses)
-                save_obj_to_disk(path=Path(image_plot_folder_path, 'tokens_mask'), obj=tokens_mask)
-                # save_model(model=vit_sigmoid_model, path=Path(f'{image_plot_folder_path}', 'vit_sigmoid_model'))
-
-        minimum_predictions = get_minimum_predictions_string(image_name=image_name, total_losses=total_losses, prediction_losses=prediction_losses)
-        print(minimum_predictions)
-        save_text_to_file(path=image_plot_folder_path, file_name='minimum_predictions', text=minimum_predictions)
-
-        # save_obj_to_disk(path=Path(image_plot_folder_path, 'temp'),
-        #                  obj=x_attention[get_top_k_mimimum_values_indices(array=prediction_losses, k=1)])
+            minimum_predictions = get_minimum_predictions_string(image_name=image_name, total_losses=total_losses,
+                                                                 prediction_losses=prediction_losses)
+            print(minimum_predictions)
+            save_text_to_file(path=image_plot_folder_path, file_name='minimum_predictions', text=minimum_predictions)
+            # save_obj_to_disk(path=Path(image_plot_folder_path, 'temp'),
+            #                  obj=x_attention[get_top_k_mimimum_values_indices(array=prediction_losses, k=1)])
 
 
 experiment_name = f"head_layer_{vit_config['objective']}_lr{str(vit_config['lr']).replace('.', '_')}_temp_{vit_config['temperature']}+l1_{loss_config['l1_loss_multiplier']}+kl_loss_{loss_config['kl_loss_multiplier']}+entropy_loss_{loss_config['entropy_loss_multiplier']}+pred_loss_{loss_config['pred_loss_multiplier']}"
 
 if __name__ == '__main__':
-    log_run = configure_log(vit_config=vit_config, experiment_name=experiment_name)
     os.makedirs(name=Path(PLOTS_PATH, experiment_name), exist_ok=True)
-    optimize_params(vit_model=vit_model, criterion=objective_temp_softmax, log_run=log_run)
+    optimize_params(vit_model=vit_model, criterion=objective_temp_softmax)
