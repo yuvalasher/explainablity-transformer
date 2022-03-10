bde90f723c9edb339230091ad7bfc4ee0950a074
Yuval Asher <asheryuvala@gmail.com>
2022-03-11 01:32:10 +0200

    code to function execution

--- main/run_temp_softmax.py
+++ main/run_temp_softmax.py
@@ -6,7 +6,7 @@ import wandb
 from log_utils import get_wandb_config
 from utils.consts import *
 from pytorch_lightning import seed_everything
-from utils.transformation import pil_to_resized_tensor_transform
+from utils.transformation import image_transformations
 from typing import Callable
 
 vit_config = config['vit']
@@ -31,49 +31,43 @@ def objective_temp_softmax(output: Tensor, target: Tensor, temp: Tensor) -> Tens
     return loss
 
 
-def get_target_class_idx(correct_class_idx: int, contrastive_class_idx: int = None) -> int:
-    target_class_idx = contrastive_class_idx if contrastive_class_idx is not None else correct_class_idx
-    return target_class_idx
+# def get_target_class_idx(correct_class_idx: int, contrastive_class_idx: int = None) -> int:
+#     target_class_idx = contrastive_class_idx if contrastive_class_idx is not None else correct_class_idx
+#     return target_class_idx
 
 
 def optimize_params(vit_model: ViTForImageClassification, criterion: Callable):
     for idx, image_dict in enumerate(vit_config['images']):
-        image_name, correct_class_idx, contrastive_class_idx = image_dict['image_name'], image_dict['correct_class'], \
-                                                               image_dict['contrastive_class']
+        image_name, correct_class_idx, contrastive_class_idx = get_image_spec(image_dict)
         wandb_config = get_wandb_config(vit_config=vit_config, experiment_name=experiment_name, image_name=image_name)
-        with wandb.init(project=config['general']['wandb_project'], entity=config['general']['wandb_entity'], config=wandb_config) as run:
+        with wandb.init(project=config['general']['wandb_project'], entity=config['general']['wandb_entity'],
+                        config=wandb_config) as run:
             vit_sigmoid_model = handle_model_config_and_freezing_for_task(
                 model=load_ViTModel(vit_config, model_type='softmax_temp'),
                 freezing_transformer=vit_config['freezing_transformer'])
-            print_number_of_trainable_and_not_trainable_params(model=vit_sigmoid_model, model_name='soft_temp')
             optimizer = optim.Adam([vit_sigmoid_model.vit.encoder.x_attention], lr=vit_config['lr'])
 
             image_plot_folder_path = get_and_create_image_plot_folder_path(images_folder_path=IMAGES_FOLDER_PATH,
                                                                            experiment_name=experiment_name,
                                                                            image_name=image_name)
-            save_text_to_file(path=image_plot_folder_path, file_name='metrics_url',
-                              text=run.url) if run is not None else ''
+
             image = get_image_from_path(Path(IMAGES_FOLDER_PATH, image_name))
             inputs = feature_extractor(images=image, return_tensors="pt")
-            original_transformed_image = pil_to_resized_tensor_transform(image)
+            original_transformed_image = image_transformations(image)
             target = vit_model(**inputs)
-            target_class_idx = torch.argmax(target.logits[0]).item()
-            plot_different_visualization_methods(path=image_plot_folder_path, inputs=inputs,
-                                                 patch_size=vit_config['patch_size'], vit_config=vit_config)
-            # target_class_idx = get_target_class_idx(correct_class_idx=correct_class_idx,
-            #                                         contrastive_class_idx=contrastive_class_idx)
+            target_class_idx = torch.argmax(target.logits[0])
+
             total_losses, prediction_losses, correct_class_logits, correct_class_probs, tokens_mask, x_attention = [], [], [], [], [], []
 
-            mean_folder, median_folder, max_folder, min_folder, temp_tokens_folder, temp_tokens_mean_folder, temp_tokens_median_folder, temp_tokens_max_folder, temp_tokens_min_folder = create_folders(
-                image_plot_folder_path=image_plot_folder_path)
-
+            max_folder, mean_folder, median_folder, min_folder, objects_path, temp_tokens_max_folder, \
+            temp_tokens_mean_folder, temp_tokens_median_folder, temp_tokens_min_folder = start_run(
+                model=vit_sigmoid_model, image_plot_folder_path=image_plot_folder_path, inputs=inputs, run=run)
             for iteration_idx in tqdm(range(vit_config['num_steps'])):
                 optimizer.zero_grad()
                 output = vit_sigmoid_model(**inputs)
-                correct_class_logits.append(output.logits[0][target_class_idx].item())
-                correct_class_probs.append(F.softmax(output.logits[0], dim=-1)[target_class_idx].item())
-                prediction_losses.append(ce_loss(output.logits, torch.argmax(target.logits).unsqueeze(0)) * loss_config[
-                    'pred_loss_multiplier'])
+
+                correct_class_logit, correct_class_prob, prediction_loss = get_iteration_target_class_stats(
+                    output=output, target_class_idx=target_class_idx)
                 x_attention.append(vit_sigmoid_model.vit.encoder.x_attention.clone())
                 loss = criterion(output=output.logits, target=target.logits,
                                  temp=vit_sigmoid_model.vit.encoder.x_attention)
@@ -95,22 +89,18 @@ def optimize_params(vit_model: ViTForImageClassification, criterion: Callable):
                                                                                         temp_tokens_min_folder=temp_tokens_min_folder,
                                                                                         vit_sigmoid_model=vit_sigmoid_model)
 
+                correct_class_logits.append(correct_class_logit)
+                correct_class_probs.append(correct_class_logit)
+                prediction_losses.append(prediction_loss)
                 total_losses.append(loss.item())
                 tokens_mask.append(cls_attentions_probs.clone())
                 optimizer.step()
 
-                if is_iteration_to_action(iteration_idx=iteration_idx, action='save'):
-                    objects_dict = {'losses': prediction_losses, 'total_losses': total_losses,
-                                    'tokens_mask': tokens_mask,
-                                    'temp': vit_sigmoid_model.vit.encoder.x_attention.clone()}
-                    objects_path = create_folder(Path(image_plot_folder_path, 'objects'))
-                    save_objects(path=objects_path, objects_dict=objects_dict)
-
-                    get_minimum_prediction_string_and_write_to_disk(image_plot_folder_path=image_plot_folder_path,
-                                                                    image_name=image_name, total_losses=total_losses,
-                                                                    prediction_losses=prediction_losses,
-                                                                    correct_class_logits=correct_class_logits,
-                                                                    correct_class_probs=correct_class_probs)
+                end_iteration(correct_class_logits=correct_class_logits, correct_class_probs=correct_class_probs,
+                              image_name=image_name, image_plot_folder_path=image_plot_folder_path,
+                              iteration_idx=iteration_idx, objects_path=objects_path,
+                              prediction_losses=prediction_losses, tokens_mask=tokens_mask, total_losses=total_losses,
+                              vit_sigmoid_model=vit_sigmoid_model)
 
 
 if __name__ == '__main__':
