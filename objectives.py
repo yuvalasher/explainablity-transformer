1c07d4c41ac97014d0168495d4858670ad8862ce
Yuval Asher <asheryuvala@gmail.com>
2022-03-18 11:58:55 +0200

    disable prints

--- objectives.py
+++ objectives.py
@@ -14,9 +14,10 @@ def objective_temp_softmax(output: Tensor, target: Tensor, temp: Tensor,
     prediction_loss = ce_loss(output, target_idx.unsqueeze(0)) * loss_config['pred_loss_multiplier']
     entropy_loss = entropy(F.softmax(temp, dim=-1)) * loss_config['entropy_loss_multiplier']
     l1_loss = torch.mean(torch.abs(temp)) * loss_config['l1_loss_multiplier']
-    print(f'entropy: {entropy_loss}, prediction_loss: {prediction_loss}, l1_loss: {l1_loss}')
     loss = entropy_loss + prediction_loss + l1_loss
-    print(f'loss: {loss}, max_temp: {torch.max(temp)}, min_temp: {torch.min(temp)}')
+    if vit_config['verbose']:
+        print(f'entropy: {entropy_loss}, prediction_loss: {prediction_loss}, l1_loss: {l1_loss}')
+        print(f'loss: {loss}, max_temp: {torch.max(temp)}, min_temp: {torch.min(temp)}')
     log(loss=loss, entropy_loss=entropy_loss, l1_loss=l1_loss, prediction_loss=prediction_loss, x_attention=temp, output=output,
         target=target, contrastive_class_idx=target_idx.item())
     return loss
