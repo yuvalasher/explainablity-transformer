b7e111dc39b3d7a4270b9db6e783761aa0077e1d
Yuval Asher <asheryuvala@gmail.com>
2022-03-18 11:40:25 +0200

    Update setup, config (also change folder for DGX) and add pictures for the DGX server

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setup(
     version='0.1.0',
     author='Yuval Asher',
     author_email='asheryuvala@gmail.com',
-    packages=['config', 'datasets', 'evaluation', 'feature_extractor', 'main', 'models', 'nlp', 'pickles', 'utils'],
+    packages=['config', 'datasets', 'evaluation', 'feature_extractor', 'main', 'models', 'nlp', 'utils'],
     scripts=[],
     install_requires=[
-        "zmq"
     ],
 )
