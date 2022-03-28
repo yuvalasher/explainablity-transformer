bc331f934f7b9d95d4dc77c1fb15b1c22a99c7ff
Yuval Asher <asheryuvala@gmail.com>
2022-03-28 17:54:03 +0300

    naming

--- models/modeling_vit.py
+++ models/modeling_vit.py
@@ -604,7 +604,7 @@ class ViTPooler(nn.Module):
     """,
     VIT_START_DOCSTRING,
 )
-class ViTBasicForDinoForImageClassification(ViTPreTrainedModel):
+class ViTBasicForForImageClassification(ViTPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
 
