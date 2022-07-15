830d452b82b8f4230b3ebb9f8815eae3315df0f6
Yuval Asher <asheryuvala@gmail.com>
2022-07-15 18:32:46 +0300

    dataset up to 10000 records

--- main/seg_classification/image_token_dataset.py
+++ main/seg_classification/image_token_dataset.py
@@ -19,7 +19,7 @@ class ImageSegDataset(Dataset):
     ):
         self.feature_extractor = feature_extractor
         self.images_path = images_path
-        self.images_name = list(Path(images_path).iterdir())
+        self.images_name = list(Path(images_path).iterdir())[:10000]
 
     def __len__(self):
         return len(self.images_name)
@@ -27,7 +27,6 @@ class ImageSegDataset(Dataset):
     def __getitem__(self, index: int):
         image_name = os.path.basename(self.images_name[index])
         image = get_image_from_path(path=Path(self.images_path, image_name))
-        # labels = data_row[LABEL_COLUMNS]
         inputs, original_transformed_image = get_image_and_inputs_and_transformed_image(image=image,
                                                                                         feature_extractor=self.feature_extractor)
 
