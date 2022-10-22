Add entropy_loss (for softmax) - Add neg_ce loss + opposite mask - Add updated config due activation_function - Add saving checkpoints due the auc  - Fixed normalize in model_opt -

main/seg_classification/image_classification_with_token_classification_model.py
@@ -84,7 +84,8 @@ class LossLoss:
         self.mask_loss_mul = loss_multipliers["mask_loss_mul"]
         print(f"loss multipliers: {self.mask_loss_mul}; {self.prediction_loss_mul}")
 
-    def __call__(self, output: Tensor, target: Tensor, tokens_mask: Tensor, output_neg=None) -> LossLossOutput:
+    def __call__(self, output: Tensor, target: Tensor, tokens_mask: Tensor,
+                 neg_output: Tensor = None) -> LossLossOutput:
         """
         Objective 1 - Keep the classification as original with as much as dark tokens
         This will be applied on the token classification by encourage the sigmoid to go to zero & CE with the original
@@ -97,14 +98,16 @@ class LossLoss:
         if self.mask_loss == "l1":
             mask_loss = l1_loss(tokens_mask)
         if self.mask_loss == "entropy_softmax":
-            tokens_mask_reshape = tokens_mask.reshape(tokens_mask.shape[0],-1) # From (32,1,14,14) --> (32,196) - easy for compute entropy.
-            d = torch.distributions.Categorical(tokens_mask_reshape+10e-8)
-            normalized_entropy = d.entropy() / np.log(d.param_shape[-1])
-            mask_loss = normalized_entropy.mean()
+            assert vit_config['activation_function'] == 'softmax', \
+                "The activation_function must be softmax!!"
+            mask_loss = self.entropy_loss(mask_loss, tokens_mask)
 
-        pred_loss = prediction_loss(output=output, target=target)
-        # if output_neg:
-        #     continue
+        pred_pos_loss = prediction_loss(output=output, target=target)
+
+        if loss_config['is_ce_neg']:
+            pred_neg_loss = -1 * prediction_loss(output=neg_output, target=target)
+            pred_loss = (pred_pos_loss + pred_neg_loss) / 2
+
         prediction_loss_multiplied = self.prediction_loss_mul * pred_loss
         mask_loss_multiplied = self.mask_loss_mul * mask_loss
         loss = prediction_loss_multiplied + mask_loss_multiplied
@@ -117,6 +120,14 @@ class LossLoss:
             pred_loss=pred_loss,
             mask_loss=mask_loss,
         )
+
+    def entropy_loss(self, mask_loss, tokens_mask):
+        tokens_mask_reshape = tokens_mask.reshape(tokens_mask.shape[0],
+                                                  -1)  # From (32,1,14,14) --> (32,196) - easy for compute entropy.
+        d = torch.distributions.Categorical(tokens_mask_reshape + 10e-8)
+        normalized_entropy = d.entropy() / np.log(d.param_shape[-1])
+        mask_loss = normalized_entropy.mean()
+        return mask_loss
 
 
 @dataclass
@@ -143,6 +154,7 @@ class ImageClassificationWithTokenClassificationModel(pl.LightningModule):
             n_classes: int = 1000,
             n_patches: int = 196,
             batch_size: int = 8,
+            p: int = 1
     ):
         super().__init__()
         self.vit_for_classification_image = vit_for_classification_image
@@ -155,6 +167,7 @@ class ImageClassificationWithTokenClassificationModel(pl.LightningModule):
         self.feature_extractor = feature_extractor
         self.is_clamp_between_0_to_1 = is_clamp_between_0_to_1
         self.plot_path = plot_path
+        self.p = p
 
     def normalize_mask_values(self, mask, is_clamp_between_0_to_1: bool):
         if is_clamp_between_0_to_1:
@@ -173,24 +186,32 @@ class ImageClassificationWithTokenClassificationModel(pl.LightningModule):
     def forward(self, inputs, image_resized) -> ImageClassificationWithTokenClassificationModelOutput:
         vit_cls_output = self.vit_for_classification_image(inputs)
         interpolated_mask, tokens_mask = self.vit_for_patch_classification(inputs)
-        if vit_config["is_relu_segmentation"] or vit_config["is_sigmoid_segmentation"] or vit_config[
-            "is_softmax_segmentation"]:
+        if vit_config["activation_function"]:
             interpolated_mask_normalized = interpolated_mask
         else:
             interpolated_mask_normalized = self.normalize_mask_values(mask=interpolated_mask.clone(),
                                                                       is_clamp_between_0_to_1=self.is_clamp_between_0_to_1)
+
+        # interpolated_mask_normalized = interpolated_mask_normalized ** self.p
+
         masked_image = image_resized * interpolated_mask_normalized
         masked_image_inputs = self.normalize_image(masked_image)
         vit_masked_output: SequenceClassifierOutput = self.vit_for_classification_image(masked_image_inputs)
+        vit_masked_output_logits = vit_masked_output.logits
 
         ### NEGATIVE MASK
-        masked_neg_image = image_resized * (1 - interpolated_mask_normalized)
-        masked_neg_image_inputs = self.normalize_image(masked_neg_image)
-        vit_masked_neg_output: SequenceClassifierOutput = self.vit_for_classification_image(masked_neg_image_inputs)
-        #
+        if loss_config['is_ce_neg']:
+            masked_neg_image = image_resized * (1 - interpolated_mask_normalized)
+            masked_neg_image_inputs = self.normalize_image(masked_neg_image)
+            vit_masked_neg_output: SequenceClassifierOutput = self.vit_for_classification_image(masked_neg_image_inputs)
+
+        vit_masked_neg_output_logits = vit_masked_neg_output.logits if vit_config['is_ce_neg'] else None
+
         lossloss_output = self.criterion(
-            output=vit_masked_output.logits, target=vit_cls_output.logits, tokens_mask=tokens_mask
+            output=vit_masked_output_logits, neg_output=vit_masked_neg_output_logits, target=vit_cls_output.logits,
+            tokens_mask=tokens_mask
         )
+
         return ImageClassificationWithTokenClassificationModelOutput(
             lossloss_output=lossloss_output,
             vit_masked_output=vit_masked_output,

main/seg_classification/image_classification_with_token_classification_model_opt.py
@@ -15,6 +15,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import wandb
 from torch.optim import AdamW
+from tqdm import tqdm
 from transformers import get_linear_schedule_with_warmup
 
 from config import config
@@ -24,6 +25,7 @@ from evaluation.perturbation_tests.seg_cls_perturbation_tests import (
 )
 from feature_extractor import ViTFeatureExtractor
 from utils import save_obj_to_disk
+from utils.metrices import batch_pix_accuracy, batch_intersection_union, get_ap_scores, get_f1_scores
 from vit_utils import visu, get_loss_multipliers
 from models.modeling_vit_patch_classification import ViTForMaskGeneration
 from transformers import ViTForImageClassification
@@ -86,7 +88,8 @@ class LossLoss:
         self.mask_loss_mul = loss_multipliers["mask_loss_mul"]
         print(f"loss multipliers: {self.mask_loss_mul}; {self.prediction_loss_mul}")
 
-    def __call__(self, output: Tensor, target: Tensor, tokens_mask: Tensor) -> LossLossOutput:
+    def __call__(self, output: Tensor, target: Tensor, tokens_mask: Tensor,
+                 neg_output: Tensor = None) -> LossLossOutput:
         """
         Objective 1 - Keep the classification as original with as much as dark tokens
         This will be applied on the token classification by encourage the sigmoid to go to zero & CE with the original
@@ -96,10 +99,18 @@ class LossLoss:
         """
         if self.mask_loss == "bce":
             mask_loss = encourage_token_mask_to_prior_loss(tokens_mask=tokens_mask, prior=0)
-        else:
+        if self.mask_loss == "l1":
             mask_loss = l1_loss(tokens_mask)
+        if self.mask_loss == "entropy_softmax":
+            assert vit_config['activation_function'] == 'softmax', \
+                "The activation_function must be softmax!!"
+            mask_loss = self.entropy_loss(mask_loss, tokens_mask)
 
-        pred_loss = prediction_loss(output=output, target=target)
+        pred_pos_loss = prediction_loss(output=output, target=target)
+
+        if loss_config['is_ce_neg']:
+            pred_neg_loss = -1 * prediction_loss(output=neg_output, target=target)
+            pred_loss = (pred_pos_loss + pred_neg_loss) / 2
 
         prediction_loss_multiplied = self.prediction_loss_mul * pred_loss
         mask_loss_multiplied = self.mask_loss_mul * mask_loss
@@ -114,6 +125,13 @@ class LossLoss:
             mask_loss=mask_loss,
         )
 
+    def entropy_loss(self, mask_loss, tokens_mask):
+        tokens_mask_reshape = tokens_mask.reshape(tokens_mask.shape[0],
+                                                  -1)  # From (32,1,14,14) --> (32,196) - easy for compute entropy.
+        d = torch.distributions.Categorical(tokens_mask_reshape + 10e-8)
+        normalized_entropy = d.entropy() / np.log(d.param_shape[-1])
+        mask_loss = normalized_entropy.mean()
+        return mask_loss
 
 @dataclass
 class ImageClassificationWithTokenClassificationModelOutput:
@@ -172,21 +190,48 @@ class OptImageClassificationWithTokenClassificationModel(pl.LightningModule):
         self.auc_by_epoch = []
         self.image_idx = len(os.listdir(self.best_auc_objects_path))
 
+    def normalize_mask_values(self, mask, is_clamp_between_0_to_1: bool):
+        if is_clamp_between_0_to_1:
+            norm_mask = torch.clamp(mask, min=0, max=1)
+        else:
+            norm_mask = (mask - mask.min()) / (mask.max() - mask.min())
+        return norm_mask
+
+    def normalize_image(self, tensor, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]):
+        dtype = tensor.dtype
+        mean = torch.as_tensor(mean, dtype=dtype, device=tensor.device)
+        std = torch.as_tensor(std, dtype=dtype, device=tensor.device)
+        tensor.sub_(mean[None, :, None, None]).div_(std[None, :, None, None])
+        return tensor
+
     def forward(self, inputs, image_resized) -> ImageClassificationWithTokenClassificationModelOutput:
         vit_cls_output = self.vit_for_classification_image(inputs)
         interpolated_mask, tokens_mask = self.vit_for_patch_classification(inputs)
         # TODO -
-        if vit_config["is_relu_segmentation"] or vit_config["is_sigmoid_segmentation"]:
+        if vit_config["activation_function"]:
             interpolated_mask_normalized = interpolated_mask
         else:
             interpolated_mask_normalized = self.normalize_mask_values(mask=interpolated_mask.clone(),
                                                                       is_clamp_between_0_to_1=self.is_clamp_between_0_to_1)
+
         masked_image = image_resized * interpolated_mask_normalized
-        # masked_image = inputs * interpolated_mask
-        vit_masked_output: SequenceClassifierOutput = self.vit_for_classification_image(masked_image)
+        masked_image_inputs = self.normalize_image(masked_image)
+        vit_masked_output: SequenceClassifierOutput = self.vit_for_classification_image(masked_image_inputs)
+        vit_masked_output_logits = vit_masked_output.logits
+
+        ### NEGATIVE MASK
+        if loss_config['is_ce_neg']:
+            masked_neg_image = image_resized * (1 - interpolated_mask_normalized)
+            masked_neg_image_inputs = self.normalize_image(masked_neg_image)
+            vit_masked_neg_output: SequenceClassifierOutput = self.vit_for_classification_image(masked_neg_image_inputs)
+
+        vit_masked_neg_output_logits = vit_masked_neg_output.logits if vit_config['is_ce_neg'] else None
+
         lossloss_output = self.criterion(
-            output=vit_masked_output.logits, target=vit_cls_output.logits, tokens_mask=tokens_mask
+            output=vit_masked_output_logits, neg_output=vit_masked_neg_output_logits, target=vit_cls_output.logits,
+            tokens_mask=tokens_mask
         )
+
         return ImageClassificationWithTokenClassificationModelOutput(
             lossloss_output=lossloss_output,
             vit_masked_output=vit_masked_output,
@@ -228,8 +273,8 @@ class OptImageClassificationWithTokenClassificationModel(pl.LightningModule):
                                           epoch_idx=self.current_epoch,
                                           )
 
-            # self.visualize_images_by_outputs(outputs=outputs)
             if self.run_base_model_only or auc < AUC_STOP_VALUE:
+                # self.visualize_images_by_outputs(outputs=outputs)
                 self.trainer.should_stop = True
 
         else:
@@ -246,6 +291,7 @@ class OptImageClassificationWithTokenClassificationModel(pl.LightningModule):
             "image_mask": images_mask,
             "image_resized": image_resized,
             "patches_mask": output.tokens_mask,
+            "auc": self.best_auc
         }
 
     def validation_step(self, batch, batch_idx):
@@ -315,12 +361,13 @@ class OptImageClassificationWithTokenClassificationModel(pl.LightningModule):
     def visualize_images_by_outputs(self, outputs):
         image = outputs[0]["resized_and_normalized_image"].detach().cpu()
         mask = outputs[0]["patches_mask"].detach().cpu()
+        auc = outputs[0]['auc']
         image = image if len(image.shape) == 3 else image.squeeze(0)
         mask = mask if len(mask.shape) == 3 else mask.squeeze(0)
         visu(
             original_image=image,
             transformer_attribution=mask,
-            file_name=Path(self.best_auc_plot_path, f"{str(self.image_idx)}").resolve(),
+            file_name=Path(self.best_auc_plot_path, f"{str(self.image_idx)}__AUC_{auc}").resolve(),
         )
 
     def validation_epoch_end(self, outputs):
@@ -401,12 +448,14 @@ class OptImageClassificationWithTokenClassificationModel_Segmentation(pl.Lightni
             best_auc_objects_path: str,
             best_auc_plot_path: str,
             checkpoint_epoch_idx: int,
+            model_runtype: str,
             criterion: LossLoss = LossLoss(),
             emb_size: int = 768,
             n_classes: int = 1000,
             n_patches: int = 196,
             batch_size: int = 8,
-            run_base_model_only: bool = False
+            run_base_model_only: bool = False,
+
     ):
         super().__init__()
         self.vit_for_classification_image = vit_for_classification_image

main/segmentation_eval/amit_test.py
@@ -1,16 +1,18 @@
 import os
 import sys
 from pathlib import Path
-
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 import yaml
 from icecream import ic
+
+from main.segmentation_eval.ViT_explanation_generator import LRP
 
 os.chdir('/home/amiteshel1/Projects/explainablity-transformer-cv/')
 
 print('START !')
 sys.path.append('/home/amiteshel1/Projects/explainablity-transformer-cv/')
 
-from main.segmentation_eval.ViT_explanation_generator import LRP
 import numpy as np
 import torch
 import torchvision.transforms as transforms
@@ -30,12 +32,10 @@ from config import config
 from main.seg_classification.image_classification_with_token_classification_model_opt import \
     OptImageClassificationWithTokenClassificationModel, OptImageClassificationWithTokenClassificationModel_Segmentation
 from utils import render
-from utils.saver import Saver
 from utils.iou import IoU
 
-from data.imagenet import Imagenet_Segmentation, Imagenet_Segmentation_Loop
+from data.imagenet import Imagenet_Segmentation
 
-from sklearn.metrics import precision_recall_curve
 import matplotlib.pyplot as plt
 
 import torch.nn.functional as F
@@ -232,7 +232,7 @@ def eval_results_per_res(Res, index, image=None, labels=None, q=-1):
 
 # hyperparameters
 num_workers = 0
-batch_size = 1
+
 
 cls = ['airplane',
        'bicycle',
@@ -293,15 +293,14 @@ def save_heatmap_and_seg_mask(Res, index, exp_name):
 def calculate_metrics_segmentations(epochs, ds, q: int):
     total_inter, total_union, total_correct, total_label = np.int64(0), np.int64(0), np.int64(0), np.int64(0)
     total_ap, total_f1 = [], []
-
     predictions, targets = [], []
 
-    for batch_idx in epochs:
+    for batch_idx in tqdm(epochs, position=0, leave=True, total=len(epochs)):
         img, labels, image_resized = ds[batch_idx]
 
         Res = lrp.generate_LRP(image_resized.unsqueeze(0).cuda(), start_layer=1,
                                method="transformer_attribution").reshape(
-            batch_size, 1, 14, 14)
+            1, 1, 14, 14)
 
         Res = torch.nn.functional.interpolate(Res, scale_factor=16, mode='bilinear').cuda()
 
@@ -325,12 +324,7 @@ def calculate_metrics_segmentations(epochs, ds, q: int):
         mIoU = IoU.mean()
         mAp = np.mean(total_ap)
         mF1 = np.mean(total_f1)
-        # if (batch_idx % 100 == 0) or (batch_idx == epochs[-1]):
-        #     print('Curr epoch: ', batch_idx, ' Method = ', method)
-        #     print("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
-        #     print("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
-        #     print("Mean AP over %d classes: %.4f\n" % (2, mAp))
-        #     print("Mean F1 over %d classes: %.4f\n" % (2, mF1))
+
     return mIoU, pixAcc, mAp, mF1
 
 
@@ -343,6 +337,24 @@ def plot_metric(q_arr, metric_a, metric_b, metrics_title, n_samples):
     plt.savefig(
         f'/home/amiteshel1/Projects/explainablity-transformer-cv/amit_th_plots/{metrics_title}__{n_samples}.png')
     plt.close()
+
+
+def init_get_normalize_and_trns():
+    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    test_img_trans = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    test_img_trans_only_resize = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+    test_lbl_trans = transforms.Compose([
+        transforms.Resize((224, 224), Image.NEAREST),
+    ])
+
+    return test_img_trans, test_img_trans_only_resize, test_lbl_trans
 
 
 if __name__ == '__main__':
@@ -367,101 +379,49 @@ if __name__ == '__main__':
 
     parser.add_argument('--imagenet-seg-path', type=str, required=True)
     args = parser.parse_args()
-
-    args.checkname = args.method + '_' + args.arc
-
-    alpha = 2
+    args.save_img = False
 
     cuda = torch.cuda.is_available()
     device = torch.device("cuda" if cuda else "cpu")
 
-    # Define Saver
-    saver = Saver(args)
-    saver.results_dir = os.path.join(saver.experiment_dir, 'results')
-    if not os.path.exists(saver.results_dir):
-        os.makedirs(saver.results_dir)
-    if not os.path.exists(os.path.join(saver.results_dir, 'input')):
-        os.makedirs(os.path.join(saver.results_dir, 'input'))
-    if not os.path.exists(os.path.join(saver.results_dir, 'explain')):
-        os.makedirs(os.path.join(saver.results_dir, 'explain'))
-
-    args.exp_img_path = os.path.join(saver.results_dir, 'explain/img')
-    if not os.path.exists(args.exp_img_path):
-        os.makedirs(args.exp_img_path)
-    args.exp_np_path = os.path.join(saver.results_dir, 'explain/np')
-    if not os.path.exists(args.exp_np_path):
-        os.makedirs(args.exp_np_path)
-
-    with open(os.path.join('/home/amiteshel1/Projects/explainablity-transformer-cv/amit_th_plots', 'config.yaml'),
-              'w') as f:
-        yaml.dump(config, f)
-
     # Data
-    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    test_img_trans = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        normalize,
-    ])
+    batch_size = 32
 
-    test_img_trans_only_resize = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor()
-    ])
-    test_lbl_trans = transforms.Compose([
-        transforms.Resize((224, 224), Image.NEAREST),
-    ])
-
+    test_img_trans, test_img_trans_only_resize, test_lbl_trans = init_get_normalize_and_trns()
     ds = Imagenet_Segmentation(args.imagenet_seg_path,
+                               batch_size=batch_size,
                                transform=test_img_trans,
                                transform_resize=test_img_trans_only_resize, target_transform=test_lbl_trans)
-
-    ################################################
-    # Model
+    # #### HILA --- LRP
+    # model_LRP = vit_LRP(pretrained=True).cuda()
+    # model_LRP.eval()
+    # lrp = LRP(model_LRP)
+    # epochs = range(len(ds))[:20]
+    # mIoU_b, pixAcc_b, mAp_b, mF1_b = [], [], [], []
+    # mIoU, pixAcc, mAp, mF1 = calculate_metrics_segmentations(epochs, ds, q=-1)
+    # print('HILA')
+    # print("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
+    # print("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
+    # print("Mean AP over %d classes: %.4f\n" % (2, mAp))
+    # print("Mean F1 over %d classes: %.4f\n" % (2, mF1))
+    # print('FINISH !!')
 
     vit_config = config["vit"]
     loss_config = vit_config["seg_cls"]["loss"]
     seed_everything(config["general"]["seed"])
     ImageFile.LOAD_TRUNCATED_IMAGES = True
     gc.collect()
-    OBT_OBJECTS_PLOT_FOLDER_NAME = 'objects_png'
-    OBT_OBJECTS_FOLDER_NAME = 'objects_pkl'
-
     loss_multipliers = get_loss_multipliers(loss_config=loss_config)
-    exp_name = f'aa_direct_opt_from_ckpt_80_pred_{loss_multipliers["prediction_loss_mul"]}_mask_l_{loss_config["mask_loss"]}_{loss_multipliers["mask_loss_mul"]}_sigmoid_{vit_config["is_sigmoid_segmentation"]}_train_n_samples_{vit_config["seg_cls"]["train_n_label_sample"] * 1000}_lr_{vit_config["lr"]}_mlp_classifier_{vit_config["is_mlp_on_segmentation"]}_is_relu_{vit_config["is_relu_segmentation"]}'
-
-    plot_path = Path(vit_config["plot_path"], exp_name)
-    # CKPT_PATH = "/home/yuvalas/explainability/research/checkpoints/token_classification/seg_cls; pred_l_1_mask_l_l1_80_sigmoid_False_freezed_seg_transformer_False_train_n_samples_6000_lr_0.002_mlp_classifier_True/None/checkpoints/epoch=3-step=751.ckpt"
     CKPT_PATH = "/home/amiteshel1/Projects/explainablity-transformer-cv/research/checkpoints/token_classification/seg_cls; amit__pred_1_mask_l_bce_50_sigmoid_True_train_n_samples_6000_lr_0.002_mlp_classifier_True_is_relu_False/None/checkpoints/epoch=4--val/epoch_auc=19.940.ckpt"
     CHECKPOINT_EPOCH_IDX = 5  # TODO - pay attention !!!
     RUN_BASE_MODEL = vit_config[
-        'run_base_model']  # TODO - Need to pay attention! If True, Running only forward of the image to create visualization of the base model
-
-    BASE_AUC_OBJECTS_PATH = Path(EXPERIMENTS_FOLDER_PATH, vit_config['evaluation'][
-        'experiment_folder_name'])  # /home/yuvalas/explainability/research/experiments/seg_cls/
-
-    EXP_NAME = 'del_fixed_ft_50000_new_model_seg_only_base_new'  # TODO - pay attention !!!
-
-    EXP_PATH = Path(BASE_AUC_OBJECTS_PATH, EXP_NAME)
-    os.makedirs(EXP_PATH, exist_ok=True)
-    ic(EXP_PATH)
-
-    BEST_AUC_PLOT_PATH = Path(BASE_AUC_OBJECTS_PATH, EXP_NAME, 'opt_model', OBT_OBJECTS_PLOT_FOLDER_NAME)
-    BEST_AUC_OBJECTS_PATH = Path(BASE_AUC_OBJECTS_PATH, EXP_NAME, 'opt_model', OBT_OBJECTS_FOLDER_NAME)
-
-    os.makedirs(BEST_AUC_PLOT_PATH, exist_ok=True)
-    os.makedirs(BEST_AUC_OBJECTS_PATH, exist_ok=True)
-
-    BASE_MODEL_BEST_AUC_PLOT_PATH = Path(BASE_AUC_OBJECTS_PATH, EXP_NAME, 'base_model', OBT_OBJECTS_PLOT_FOLDER_NAME)
-    BASE_MODEL_BEST_AUC_OBJECTS_PATH = Path(BASE_AUC_OBJECTS_PATH, EXP_NAME, 'base_model', OBT_OBJECTS_FOLDER_NAME)
-    os.makedirs(BASE_MODEL_BEST_AUC_PLOT_PATH, exist_ok=True)
-    os.makedirs(BASE_MODEL_BEST_AUC_OBJECTS_PATH, exist_ok=True)
+        'run_base_model']  # TODO If True, Running only forward of the image to create visualization of the base model
 
     feature_extractor, _ = load_feature_extractor_and_vit_model(
         vit_config=vit_config,
         model_type="vit-basic",
         is_wolf_transforms=vit_config["is_wolf_transforms"],
-    )  # TODO if vit-for-dino is relevant
+    )
 
     vit_for_classification_image, vit_for_patch_classification = load_vit_pretrained(
         model_name=vit_config["model_name"])
@@ -472,86 +432,50 @@ if __name__ == '__main__':
         batch_size=vit_config["batch_size"],
     )
 
+    metric = IoU(2, ignore_index=-1)
+
+
     model = OptImageClassificationWithTokenClassificationModel_Segmentation(
         vit_for_classification_image=vit_for_classification_image,
         vit_for_patch_classification=vit_for_patch_classification,
         feature_extractor=feature_extractor,
-        plot_path=plot_path,
+        plot_path='',
         warmup_steps=warmup_steps,
         total_training_steps=total_training_steps,
-        batch_size=vit_config["batch_size"],
-        best_auc_objects_path=BEST_AUC_OBJECTS_PATH,
+        batch_size=batch_size,
+        best_auc_objects_path='',
         checkpoint_epoch_idx=CHECKPOINT_EPOCH_IDX,
-        best_auc_plot_path=BEST_AUC_PLOT_PATH,
-        run_base_model_only=RUN_BASE_MODEL
+        best_auc_plot_path='',
+        run_base_model_only=RUN_BASE_MODEL,
+        model_runtype='test'
     )
     model = freeze_multitask_model(
         model=model,
         freezing_transformer=vit_config["freezing_transformer"],
     )
-    metric = IoU(2, ignore_index=-1)
 
-    #### HILA --- LRP
-    model_LRP = vit_LRP(pretrained=True).cuda()
-    model_LRP.eval()
-    lrp = LRP(model_LRP)
-
-    print('START TRAINING !')
-    args.save_img = False
-    epochs = range(len(ds))[:1]
-    print(f'Run over {len(epochs)} samples')
-    mIoU_a, pixAcc_a, mAp_a, mF1_a = [], [], [], []
-    mIoU_b, pixAcc_b, mAp_b, mF1_b = [], [], [], []
-    q_arr = [-1]  # np.arange(0, 1, 0.1)
-    for q in tqdm(q_arr):
-        mIoU, pixAcc, mAp, mF1 = calculate_metrics_segmentations(epochs, ds, method='ours', q=q)
-        mIoU_a.append(mIoU)
-        pixAcc_a.append(pixAcc)
-        mAp_a.append(mAp)
-        mF1_a.append(mF1)
-        print('Ours!')
-        print("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
-        print("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
-        print("Mean AP over %d classes: %.4f\n" % (2, mAp))
-        print("Mean F1 over %d classes: %.4f\n" % (2, mF1))
-        mIoU, pixAcc, mAp, mF1 = calculate_metrics_segmentations(epochs, ds, method='hila', q=q)
-        print('HILA')
-        print("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
-        print("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
-        print("Mean AP over %d classes: %.4f\n" % (2, mAp))
-        print("Mean F1 over %d classes: %.4f\n" % (2, mF1))
-        mIoU_b.append(mIoU)
-        pixAcc_b.append(pixAcc)
-        mAp_b.append(mAp)
-        mF1_b.append(mF1)
-    print('end')
-    plot_metric(q_arr, metric_a=mIoU_a, metric_b=mIoU_b, metrics_title='mIoU', n_samples=len(epochs))
-    plot_metric(q_arr, metric_a=np.array(pixAcc_a) * 100, metric_b=np.array(pixAcc_b) * 100, metrics_title='pixAcc',
-                n_samples=len(epochs))
-    plot_metric(q_arr, metric_a=mAp_a, metric_b=mAp_b, metrics_title='mAp', n_samples=len(epochs))
-    plot_metric(q_arr, metric_a=mF1_a, metric_b=mF1_b, metrics_title='mF1', n_samples=len(epochs))
-    print('FINISH!!')
-    # print("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
-    # print("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
-    # print("Mean AP over %d classes: %.4f\n" % (2, mAp))
-    # print("Mean F1 over %d classes: %.4f\n" % (2, mF1))
-    # print("FINISH !!!!!")
-    # txtfile = os.path.join(saver.experiment_dir, 'result_mIoU_%.4f.txt' % mIoU)
-    # # txtfile = 'result_mIoU_%.4f.txt' % mIoU
-    # fh = open(txtfile, 'w')
-    #
-    # fh.write("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
-    # fh.write("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
-    # fh.write("Mean AP over %d classes: %.4f\n" % (2, mAp))
-    # fh.write("Mean F1 over %d classes: %.4f\n" % (2, mF1))
-    # fh.close()
-    #
-    # predictions = np.concatenate(predictions)
-    # targets = np.concatenate(targets)
-    # pr, rc, thr = precision_recall_curve(targets, predictions)
-    # np.save(os.path.join(saver.experiment_dir, 'precision.npy'), pr)
-    # np.save(os.path.join(saver.experiment_dir, 'recall.npy'), rc)
-    #
-    # plt.figure()
-    # plt.plot(rc, pr)
-    # plt.savefig(os.path.join(saver.experiment_dir, 'PR_curve_{}.png'.format(args.method)))
+    dl = DataLoader(ds, batch_size=batch_size, shuffle=False, num_workers=1, drop_last=False)
+    data_module = ImageSegOptDataModuleSegmentation(train_data_loader=dl)
+    trainer = pl.Trainer(
+        logger=[],
+        accelerator='gpu',
+        gpus=1,
+        devices=1,
+        num_sanity_val_steps=0,
+        check_val_every_n_epoch=100,
+        max_epochs=vit_config["n_epochs"],
+        resume_from_checkpoint=CKPT_PATH,
+        enable_progress_bar=True,
+        enable_checkpointing=False,
+        default_root_dir=vit_config["default_root_dir"],
+        weights_summary=None
+    )
+    trainer.fit(model=model, datamodule=data_module)
+    trainer.test(model=model, datamodule=data_module)
+    # trainer.fit(model=model, datamodule=data_module)
+    mIoU, pixAcc, mAp, mF1 = model.seg_results['mIoU'], model.seg_results['pixAcc'], model.seg_results['mAp'], \
+                             model.seg_results['mF1']
+    print("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
+    print("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
+    print("Mean AP over %d classes: %.4f\n" % (2, mAp))
+    print("Mean F1 over %d classes: %.4f\n" % (2, mF1))

main/segmentation_eval/power_test.py
@@ -83,94 +83,14 @@ def compute_pred(output):
     return Tt
 
 
-def eval_batch(image, labels, evaluator, index):
-    evaluator.zero_grad()
-    # Save input image
+def eval_results_per_res(Res, index, image=None, labels=None, q=0.5):
     if args.save_img:
         save_original_image_and_gt_mask(image, index, labels)
 
-    image.requires_grad = True
-
-    image = image.requires_grad_()
-    predictions = evaluator(image)
-
-    # if args.method != 'full_lrp':
-    #     # interpolate to full image size (224,224)
-    #     Res = torch.nn.functional.interpolate(Res, scale_factor=16, mode='bilinear').cuda()
-
-    # threshold between FG and BG is the mean
     Res = (Res - Res.min()) / (Res.max() - Res.min())
 
     ret = Res.mean()
-
-    Res_1 = Res.gt(ret).type(Res.type())
-    Res_0 = Res.le(ret).type(Res.type())
-
-    Res_1_AP = Res
-    Res_0_AP = 1 - Res
-
-    Res_1[Res_1 != Res_1] = 0
-    Res_0[Res_0 != Res_0] = 0
-    Res_1_AP[Res_1_AP != Res_1_AP] = 0
-    Res_0_AP[Res_0_AP != Res_0_AP] = 0
-
-    # TEST
-    pred = Res.clamp(min=args.thr) / Res.max()
-    pred = pred.view(-1).data.cpu().numpy()
-    target = labels.view(-1).data.cpu().numpy()
-    # print("target", target.shape)
-
-    output = torch.cat((Res_0, Res_1), 1)
-    output_AP = torch.cat((Res_0_AP, Res_1_AP), 1)
-
-    if args.save_img:
-        # Save predicted mask
-        mask = F.interpolate(Res_1, [64, 64], mode='bilinear')
-        mask = mask[0].squeeze().data.cpu().numpy()
-        # mask = Res_1[0].squeeze().data.cpu().numpy()
-        mask = 255 * mask
-        mask = mask.astype('uint8')
-        imageio.imsave(os.path.join(args.exp_img_path, 'mask_' + str(index) + '.jpg'), mask)
-
-        relevance = F.interpolate(Res, [64, 64], mode='bilinear')
-        relevance = relevance[0].permute(1, 2, 0).data.cpu().numpy()
-        # relevance = Res[0].permute(1, 2, 0).data.cpu().numpy()
-        hm = np.sum(relevance, axis=-1)
-        maps = (render.hm_to_rgb(hm, scaling=3, sigma=1, cmap='seismic') * 255).astype(np.uint8)
-        imageio.imsave(os.path.join(args.exp_img_path, 'heatmap_' + str(index) + '.jpg'), maps)
-
-    # Evaluate Segmentation
-    batch_inter, batch_union, batch_correct, batch_label = 0, 0, 0, 0
-    batch_ap, batch_f1 = 0, 0
-
-    # Segmentation resutls
-    correct, labeled = batch_pix_accuracy(output[0].data.cpu(), labels[0])
-    inter, union = batch_intersection_union(output[0].data.cpu(), labels[0], 2)
-    batch_correct += correct
-    batch_label += labeled
-    batch_inter += inter
-    batch_union += union
-    # print("output", output.shape)
-    # print("ap labels", labels.shape)
-    # ap = np.nan_to_num(get_ap_scores(output, labels))
-    ap = np.nan_to_num(get_ap_scores(output_AP, labels))
-    f1 = np.nan_to_num(get_f1_scores(output[0, 1].data.cpu(), labels[0]))
-    batch_ap += ap
-    batch_f1 += f1
-
-    return batch_correct, batch_label, batch_inter, batch_union, batch_ap, batch_f1, pred, target
-
-
-def eval_results_per_res(Res, index, image=None, labels=None, q=-1):
-    if args.save_img:
-        save_original_image_and_gt_mask(image, index, labels)
-
-    Res = (Res - Res.min()) / (Res.max() - Res.min())
-
-    if q == -1:
-        ret = Res.mean()
-    else:
-        ret = torch.quantile(Res, q=q)
+    # ret = torch.quantile(Res, q=q)
 
     Res_1 = Res.gt(ret).type(Res.type())
     Res_0 = Res.le(ret).type(Res.type())
@@ -290,21 +210,65 @@ def save_heatmap_and_seg_mask(Res, index, exp_name):
     return
 
 
-def calculate_metrics_segmentations(epochs, ds, q: int):
+def show_progress_of_power(Res_org):
+    for q in np.arange(1, 4, 0.5):
+        Res = Res_org ** q
+        Res = (Res - Res.min()) / (Res.max() - Res.min())
+        ret = Res.mean()
+        Res_1 = Res.gt(ret).type(Res.type())
+        Res_1[Res_1 != Res_1] = 0
+        mask = F.interpolate(Res_1, [64, 64], mode='bilinear')
+        mask = mask[0].squeeze().data.cpu().numpy()
+        # mask = Res_1[0].squeeze().data.cpu().numpy()
+        mask = 255 * mask
+        mask = mask.astype('uint8')
+        plt.imshow(mask)
+        plt.title(f'q = {q} th_value = {ret}')
+        plt.show()
+
+
+def calculate_metrics_segmentations(epochs, ds, method: str, q: int):
     total_inter, total_union, total_correct, total_label = np.int64(0), np.int64(0), np.int64(0), np.int64(0)
     total_ap, total_f1 = [], []
 
     predictions, targets = [], []
 
     for batch_idx in epochs:
-        img, labels, image_resized = ds[batch_idx]
+        img, labels, _, image_resized = ds[batch_idx]
+        ds_loop = Imagenet_Segmentation_Loop(*ds[batch_idx])
 
-        Res = lrp.generate_LRP(image_resized.unsqueeze(0).cuda(), start_layer=1,
-                               method="transformer_attribution").reshape(
-            batch_size, 1, 14, 14)
+        if method == 'ours':
+            dl = DataLoader(ds_loop, batch_size=batch_size, shuffle=False, num_workers=1, drop_last=False)
+            data_module = ImageSegOptDataModuleSegmentation(
+                feature_extractor=feature_extractor,
+                batch_idx=1,
+                train_data_loader=dl
+            )
+            trainer = pl.Trainer(
+                logger=[],
+                accelerator='gpu',
+                gpus=1,
+                devices=[1, 2],
+                num_sanity_val_steps=0,
+                check_val_every_n_epoch=100,
+                max_epochs=vit_config["n_epochs"],
+                resume_from_checkpoint=CKPT_PATH,
+                enable_progress_bar=False,
+                enable_checkpointing=False,
+                default_root_dir=vit_config["default_root_dir"],
+                weights_summary=None
+            )
+            trainer.fit(model=model, datamodule=data_module)
+            Res_org = model.best_auc_vis
 
-        Res = torch.nn.functional.interpolate(Res, scale_factor=16, mode='bilinear').cuda()
+        else:
+            Res_org = lrp.generate_LRP(image_resized.unsqueeze(0).cuda(), start_layer=1,
+                                       method="transformer_attribution").reshape(
+                batch_size, 1, 14, 14)
+            Res_org = torch.nn.functional.interpolate(Res_org, scale_factor=16, mode='bilinear').cuda()
 
+        Res = Res_org ** q
+        # show_progress_of_power(Res_org)
         correct, labeled, inter, union, ap, f1, pred, target = eval_results_per_res(Res,
                                                                                     index=batch_idx,
                                                                                     q=q,
@@ -332,6 +296,28 @@ def calculate_metrics_segmentations(epochs, ds, q: int):
         #     print("Mean AP over %d classes: %.4f\n" % (2, mAp))
         #     print("Mean F1 over %d classes: %.4f\n" % (2, mF1))
     return mIoU, pixAcc, mAp, mF1
+    # ### HILA !!
+    # Res_b = lrp.generate_LRP(image_resized.cuda(), start_layer=1, method="transformer_attribution").reshape(
+    #     batch_size, 1, 14, 14)
+    # Res_b = torch.nn.functional.interpolate(Res_b, scale_factor=16, mode='bilinear').cuda()
+    #
+    # correct_b, labeled_b, inter_b, union_b, ap_b, f1_b, pred_b, target_b = eval_results_per_res(Res_b,
+    #                                                                                             labels=labels,
+    #                                                                                             index=batch_idx,
+    #                                                                                             image=image_resized,
+    #                                                                                             q=q)
+
+    # pixAcc_a = np.float64(1.0) * correct.astype('int64') / (
+    #         np.spacing(1, dtype=np.float64) + labeled.astype('int64'))
+    # pixAcc_b = np.float64(1.0) * correct_b.astype('int64') / (
+    #         np.spacing(1, dtype=np.float64) + labeled_b.astype('int64'))
+    #
+    # image = image_resized
+    # index = batch_idx
+    # if pixAcc_a < pixAcc_b:
+    #     save_original_image_and_gt_mask(image, index, labels)
+    #     save_heatmap_and_seg_mask(Res, index, exp_name='ours')
+    #     save_heatmap_and_seg_mask(Res_b, index, exp_name='hila')
 
 
 def plot_metric(q_arr, metric_a, metric_b, metrics_title, n_samples):
@@ -341,8 +327,8 @@ def plot_metric(q_arr, metric_a, metric_b, metrics_title, n_samples):
     plt.grid()
     plt.title(f'{metrics_title} - num_samples = {n_samples}')
     plt.savefig(
-        f'/home/amiteshel1/Projects/explainablity-transformer-cv/amit_th_plots/{metrics_title}__{n_samples}.png')
-    plt.close()
+        f'/home/amiteshel1/Projects/explainablity-transformer-cv/amit_power_plots/{metrics_title}__{n_samples}.png')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -391,8 +377,8 @@ if __name__ == '__main__':
     args.exp_np_path = os.path.join(saver.results_dir, 'explain/np')
     if not os.path.exists(args.exp_np_path):
         os.makedirs(args.exp_np_path)
-
-    with open(os.path.join('/home/amiteshel1/Projects/explainablity-transformer-cv/amit_th_plots', 'config.yaml'),
+    print('saver.experiment_dir = ', saver.experiment_dir)
+    with open(os.path.join('/home/amiteshel1/Projects/explainablity-transformer-cv/amit_power_plots', 'config.yaml'),
               'w') as f:
         yaml.dump(config, f)
 
@@ -498,28 +484,18 @@ if __name__ == '__main__':
 
     print('START TRAINING !')
     args.save_img = False
-    epochs = range(len(ds))[:1]
+    epochs = range(len(ds))[:500]
     print(f'Run over {len(epochs)} samples')
     mIoU_a, pixAcc_a, mAp_a, mF1_a = [], [], [], []
     mIoU_b, pixAcc_b, mAp_b, mF1_b = [], [], [], []
-    q_arr = [-1]  # np.arange(0, 1, 0.1)
+    q_arr = np.arange(1, 4.5, 0.3)
     for q in tqdm(q_arr):
         mIoU, pixAcc, mAp, mF1 = calculate_metrics_segmentations(epochs, ds, method='ours', q=q)
         mIoU_a.append(mIoU)
         pixAcc_a.append(pixAcc)
         mAp_a.append(mAp)
         mF1_a.append(mF1)
-        print('Ours!')
-        print("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
-        print("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
-        print("Mean AP over %d classes: %.4f\n" % (2, mAp))
-        print("Mean F1 over %d classes: %.4f\n" % (2, mF1))
         mIoU, pixAcc, mAp, mF1 = calculate_metrics_segmentations(epochs, ds, method='hila', q=q)
-        print('HILA')
-        print("Mean IoU over %d classes: %.4f\n" % (2, mIoU))
-        print("Pixel-wise Accuracy: %2.2f%%\n" % (pixAcc * 100))
-        print("Mean AP over %d classes: %.4f\n" % (2, mAp))
-        print("Mean F1 over %d classes: %.4f\n" % (2, mF1))
         mIoU_b.append(mIoU)
         pixAcc_b.append(pixAcc)
         mAp_b.append(mAp)

models/modeling_vit_patch_classification.py
@@ -68,17 +68,14 @@ class ViTForMaskGeneration(ViTPreTrainedModel):
         logits = self.patch_classifier(tokens_output_reshaped)
         mask = logits.view(batch_size, -1, 1)  # logits - [batch_size, tokens_count]
 
-        if vit_config["is_relu_segmentation"] and vit_config["is_sigmoid_segmentation"]:
-            raise (
-                f"Can't is_relu_segmentation: {vit_config['is_relu_segmentation']} and is_sigmoid_segmentation: {vit_config['is_sigmoid_segmentation']}")
-
-        if vit_config["is_relu_segmentation"]:
+        if vit_config["activation_function"] == 'relu':
             mask = torch.relu(mask)
-
-        if vit_config["is_sigmoid_segmentation"]:
+        if vit_config["activation_function"] == 'sigmoid':
             mask = torch.sigmoid(mask)
-        if vit_config["is_softmax_segmentation"]:
-            mask = torch.softmax(mask,dim=1)
+        if vit_config["activation_function"] == 'softmax':
+            mask = torch.softmax(mask, dim=1)
+        if vit_config["normalize_by_max_patch"]:
+            mask = mask / mask.max(dim=1, keepdim=True)[0]
 
         mask = mask.view(batch_size, 1, 14, 14)
 