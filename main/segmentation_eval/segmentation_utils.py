00d0000bb3f16fb500bb247bc84bd7586d846a8a
Yuval Asher <asheryuvala@gmail.com>
2022-10-31 16:25:49 +0200

    prettier segmentation metrics

--- main/segmentation_eval/segmentation_utils.py
+++ main/segmentation_eval/segmentation_utils.py
@@ -1,5 +1,5 @@
 def print_segmentation_results(pixAcc: float, mAp: float, mIoU: float, mF1: float) -> None:
-    print(f"Pixel-wise-Accuracy {round(pixAcc * 100, 4)}%")
+    print(f"\nPixel-wise-Accuracy {round(pixAcc * 100, 4)}%")
     print(f"mAP {round(mAp * 100, 4)}%")
     print(f"mIoU {round(mIoU * 100, 4)}%")
     print(f"F1 {round(mF1 * 100, 4)}%")
