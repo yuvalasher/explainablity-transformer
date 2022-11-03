6699c5907592f5d6b8cf47811c415ae1a4ff8a99
Yuval Asher <asheryuvala@gmail.com>
2022-11-03 13:31:35 +0200

    add gt-targets to hila's dataset, by list of images & write images to disk & auc of images in dict pickle

--- hila_method/hila_generate_visualizations.py
+++ hila_method/hila_generate_visualizations.py
@@ -1,9 +1,10 @@
+from itertools import chain
+import pickle
 from datetime import datetime as dt
 from icecream import ic
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import ViTForImageClassification
-from evaluation.perturbation_tests.seg_cls_perturbation_tests import run_perturbation_test, eval_perturbation_test
 from hila_method.utils.ViT_LRP import vit_base_patch16_224 as vit_LRP
 from hila_method.utils.ViT_explanation_generator import LRP
 from hila_method.utils.imagenet_dataset import ImageNetDataset
@@ -22,6 +23,7 @@ import cv2
 import matplotlib.pyplot as plt
 from typing import Dict, List
 from pathlib import Path
+from evaluation.perturbation_tests.seg_cls_perturbation_tests import eval_perturbation_test
 
 seed_everything(config["general"]["seed"])
 
@@ -126,18 +128,18 @@ def compute_saliency_generator(dataloader: DataLoader):
 
 
 def visualize_outputs(outputs):
-    print(len(outputs))
-    for batch_idx, output in enumerate(outputs):
+    for image_idx, output in tqdm(enumerate(outputs)):
         for idx, (image, mask) in enumerate(
                 zip(output["image_resized"].detach().cpu(), output["patches_mask"].detach().cpu())):
             visu(
                 original_image=image,
                 transformer_attribution=mask,
-                file_name=Path(HILA_VISUAILZATION_PATH, f"hila_{batch_idx}").resolve(),
+                file_name=Path(HILA_VISUAILZATION_PATH,
+                               f"{str(output['image_idx'])}_hila_auc_{int(output['auc'])}").resolve(),
             )
 
 
-def run_adp_pic_tests_hila(vit_for_image_classification: ViTForImageClassification, images_and_masks):
+def run_adp_pic_tests_hila(vit_for_image_classification, images_and_masks):
     gt_classes_list = get_gt_classes(GT_VALIDATION_PATH_LABELS)
     start_time = dt.now()
     evaluation_metrics = infer_adp_pic_acp(vit_for_image_classification=vit_for_image_classification,
@@ -153,9 +155,23 @@ def run_adp_pic_tests_hila(vit_for_image_classification: ViTForImageClassificati
 
     print(f"timing: {(dt.now() - start_time).total_seconds()}")
 
+
 IMAGENET_VALIDATION_PATH = "/home/amiteshel1/Projects/explainablity-transformer/vit_data/"
 GT_VALIDATION_PATH_LABELS = "/home/yuvalas/explainability/data/val ground truth 2012.txt"
-from config import config
+
+
+def load_obj(path: str):
+    with open(Path(path), 'rb') as f:
+        return pickle.load(f)
+
+
+def save_obj_to_disk(path, obj) -> None:
+    if type(path) == str and path[-4:] != '.pkl':
+        path += '.pkl'
+
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
 
 if __name__ == '__main__':
     BATCH_SIZE = 1
@@ -170,20 +186,58 @@ if __name__ == '__main__':
 
     vit_for_classification_image, _ = load_vit_pretrained(model_name=MODEL_NAME)
     vit_for_classification_image = vit_for_classification_image.to(device)
-    n_samples = 5000
-    imagenet_ds = ImageNetDataset(root_dir=IMAGENET_VALIDATION_PATH, n_samples=n_samples, transform=transform)
+
+    IMAGES_IDX_BY_AUC_DIFF_BASE_OPT_MODEL_PATH = "/home/yuvalas/explainability/pickles/images_idx_by_auc_diff_base_opt_model.pkl"
+    HILA_AUC_BY_IMG_IDX_PATH = "/home/yuvalas/explainability/pickles/hila_auc_by_img_idx.pkl"
+    # PLOTS_OUTPUT_PATH = "/home/yuvalas/explainability/pickles/comparison_base_opt_models"
+
+    images_idx_by_auc_diff_base_opt_model = load_obj(path=IMAGES_IDX_BY_AUC_DIFF_BASE_OPT_MODEL_PATH)
+
+    # images_indices = []
+    # for key in images_idx_by_auc_diff_base_opt_model.keys():
+    #     images_indices.extend(images_idx_by_auc_diff_base_opt_model[key])
+    # interesting_indices_to_look = [218, 245, 456, 457, 549, 710, 729, 754, 878, 1037, 1056, 1234, 1332, 1450, 1654,
+    #                                1671,1700, 1740, 1831, 1859, 1875, 1980, 2534, 2566, 2691, 2700, 2750, 2763, 2808,
+    #                                2820,2985,3032, 3083, 3104, 3105, 3206, 3209, 3344, 3414, 3864, 4144, 4282, 4741, 4795,
+    #                                4928,5018,5022, 5078, 5086, 5102, 5186, 5196, 5237, 5596, 5939, 6018, 6027, 6324, 6734,
+    #                                6821,6877,6912, 6984, 7021, 7268, 7306, 7341, 8048, 8268, 8311, 8408, 8600, 8693, 8734,
+    #                                8741,8971,9043, 9085, 9319, 9324, 9510, 9637, 9920, 9939, 10019, 10264, 10314, 10512,
+    #                                10830,10875,10911, 11079, 11410, 11543, 11721, 11864, 11867, 12146, 12392, 12424, 12471,
+    #                                12490,12630, 12718, 12804, 12871, 12920, 13252, 13293, 13494, 13566, 13654, 13790, 14014,
+    #                                14026, 14189, 14217, 14225, 14620, 14646, 14681, 14985, 7305, 9376, 14229, 149, 193,
+    #                                215,994, 1187, 1515, 1631, 1918, 2313, 3941, 4031, 4707, 4888, 5064, 5287, 5463,
+    #                                6045,6225,6887, 7340, 7635, 8439, 8444, 9053, 9110, 9224, 10003, 11153, 11307, 11846,
+    #                                12433,12573,14514, 14703]
+    # images_indices = interesting_indices_to_look
+
+    images_indices = list(chain(*images_idx_by_auc_diff_base_opt_model.values()))
+    print(len(images_indices))
+    n_samples = len(images_indices)
+    imagenet_ds = ImageNetDataset(root_dir=IMAGENET_VALIDATION_PATH, n_samples=n_samples,
+                                  list_of_images_names=images_indices, transform=transform)
     sample_loader = torch.utils.data.DataLoader(
         imagenet_ds,
         batch_size=BATCH_SIZE,
         shuffle=False,
         num_workers=4
     )
+    hila_auc_by_img_idx = {}
     # Perturbations
     outputs = compute_saliency_and_save(dataloader=sample_loader)
-    auc = eval_perturbation_test(experiment_dir=Path(""), model=vit_for_classification_image, outputs=outputs)
-    print(f'AUC: {round(auc, 4)}% for {len(outputs)} records')
-    # visualize_outputs(outputs=outputs)
+    for idx, output in enumerate(outputs):
+        auc = eval_perturbation_test(experiment_dir=Path(""), model=vit_for_classification_image, outputs=[output])
+        output["auc"] = auc
+        output["image_idx"] = images_indices[idx]
+        # ic(images_indices[idx])
+        hila_auc_by_img_idx[images_indices[idx]] = auc
+    save_obj_to_disk(path=HILA_AUC_BY_IMG_IDX_PATH, obj=hila_auc_by_img_idx)
 
+    # print(f'AUC: {round(auc, 4)}% for {len(outputs)} records')
+    # for key in images_idx_by_auc_diff_base_opt_model.keys():
+    #     if image_idx in images_idx_by_auc_diff_base_opt_model[key]:
+    #         plot_subfolder_name = f"auc_diff_{key}"
+    # load_obj(HILA_AUC_BY_IMG_IDX_PATH)
+    visualize_outputs(outputs=outputs)
 
     # ADP & PIC
     # images_and_masks_generator = compute_saliency_generator(dataloader=sample_loader)
--- hila_method/utils/imagenet_dataset.py
+++ hila_method/utils/imagenet_dataset.py
@@ -1,29 +1,31 @@
 import os
 from pathlib import Path
+from typing import List
 
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
 
-
-# IMAGES_FOLDER_PATH = Path('/home/yuvalas/wolf/Transformer-Explainability/baselines/data')
-# IMAGES_FOLDER_PATH = Path('/home/yuvalas/explainability/data/run_hila_3000')
-
-
-# targets = list(range(3000))
+GT_VALIDATION_PATH_LABELS = "/home/yuvalas/explainability/data/val ground truth 2012.txt"
+DEFAULT_LIST_OF_IMAGES = list(range(50000))
 
 
 class ImageNetDataset(Dataset):
 
-    def __init__(self, root_dir, n_samples: int, transform=None):
+    def __init__(self, root_dir, n_samples: int, list_of_images_names: List[int] = DEFAULT_LIST_OF_IMAGES,
+                 transform=None):
         self.root_dir = root_dir
         self.transform = transform
         self.listdir = sorted(os.listdir(root_dir))
-        self.targets = list(range(len(self.listdir)))
+        self.targets = get_gt_classes(path=GT_VALIDATION_PATH_LABELS)
 
         n_samples = n_samples if n_samples > 0 else len(self.listdir)
-        self.images_name = self.listdir[:n_samples]
-        print(self.images_name[:10])
+        self.images_name = [f"ILSVRC2012_val_{str(image_idx + 1).zfill(8)}.JPEG" for image_idx in list_of_images_names]
+        self.targets = [self.targets[image_idx] for image_idx in list_of_images_names]
+        self.images_name = self.images_name[:n_samples]
+        self.targets = self.targets[:n_samples]
+        # self.images_name = self.listdir[:n_samples]
+        # print(self.images_name[:10])
         print(f"After filter images: {len(self.images_name)}")
         # print(self.images_name)
 
@@ -33,8 +35,6 @@ class ImageNetDataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.item()
-
-        # image_name = self.listdir[idx]
         image_name = self.images_name[idx]
         image_path = Path(self.root_dir, image_name)
         image = Image.open(image_path)
@@ -49,3 +49,10 @@ def show_pil_image(image):
     from matplotlib import pyplot as plt
     plt.imshow(image)
     plt.show()
+
+
+def get_gt_classes(path):
+    with open(path, 'r') as f:
+        gt_classes_list = f.readlines()
+    gt_classes_list = [int(record.split()[-1].replace('\n', '')) for record in gt_classes_list]
+    return gt_classes_list
