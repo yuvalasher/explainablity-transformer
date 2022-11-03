b33f82e4a3330840aa040523e35b753a5834bade
Yuval Asher <asheryuvala@gmail.com>
2022-11-03 21:30:56 +0200

    remove not relevant backbones

--- main/segmentation_eval/seg_stage_a.py
+++ main/segmentation_eval/seg_stage_a.py
@@ -111,14 +111,12 @@ if __name__ == '__main__':
         'run_base_model']  # TODO If True, Running only forward of the image to create visualization of the base model
 
     feature_extractor = ViTFeatureExtractor.from_pretrained(vit_config["model_name"])
-    if vit_config["model_name"] in ["google/vit-base-patch16-224", "augreg"]:
+    if vit_config["model_name"] in ["google/vit-base-patch16-224"]:
         vit_for_classification_image, vit_for_patch_classification = load_vit_pretrained(
             model_name=vit_config["model_name"])
     else:
         vit_for_classification_image = ViTForImageClassification.from_pretrained(vit_config["model_name"])
         vit_for_patch_classification = ViTForMaskGeneration.from_pretrained(vit_config["model_name"])
-    if "deit" in vit_config["model_name"].lower():
-        vit_config["is_wolf_transforms"] = True
 
     warmup_steps, total_training_steps = get_warmup_steps_and_total_training_steps(
         n_epochs=vit_config["n_epochs"],
