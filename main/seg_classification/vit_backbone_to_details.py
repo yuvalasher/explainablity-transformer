30dafd4f2d7235ae927503404a23c0ebec67e5c6
Yuval Asher <asheryuvala@gmail.com>
2022-11-05 15:34:11 +0200

    HOTFIX - mistake vit small predicted-target, target-predicted

--- main/seg_classification/vit_backbone_to_details.py
+++ main/seg_classification/vit_backbone_to_details.py
@@ -3,8 +3,8 @@ VIT_BASE_224_TARGET_CKPT_PATH = "/raid/yuvalas/checkpoints/target/lightning_logs
 
 VIT_BASE_224_TARGET_CKPT_PATH_16_155_1000_samples = "/raid/yuvalas/checkpoints/target/lightning_logs/version_12/checkpoints/epoch=47_val/epoch_auc=16.155.ckpt"
 
-VIT_SMALL_224_PREDICTED_CKPT_PATH = "/raid/yuvalas/checkpoints/target/lightning_logs/version_14/checkpoints/epoch=9_val/epoch_auc=15.490.ckpt"
-VIT_SMALL_224_TRAGET_CKPT_PATH = "/raid/yuvalas/checkpoints/predicted/lightning_logs/version_2/checkpoints/epoch=8_val/epoch_auc=15.620.ckpt"
+VIT_SMALL_224_PREDICTED_CKPT_PATH = "/raid/yuvalas/checkpoints/predicted/lightning_logs/version_2/checkpoints/epoch=8_val/epoch_auc=15.620.ckpt"
+VIT_SMALL_224_TRAGET_CKPT_PATH = "/raid/yuvalas/checkpoints/target/lightning_logs/version_14/checkpoints/epoch=9_val/epoch_auc=15.490.ckpt"
 
 # VIT_BASE_384_CKPT_PATH = "/home/yuvalas/explainability/research/checkpoints/token_classification/model_google_vit-base-patch16-384_train_uni_True_val_unif_True_activation_sigmoid__norm_by_max_p_False_pred_1_mask_l_bce_70__train_n_samples_1000_lr_0.002_mlp_classifier_True__bs_16/None/checkpoints/epoch=9_val/epoch_auc=21.750.ckpt"
 
