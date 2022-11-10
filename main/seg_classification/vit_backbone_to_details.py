3ec76cdc4a895ab8b9d533a80c6fd3ea2b20a6fe
Yuval Asher <asheryuvala@gmail.com>
2022-11-10 10:32:20 +0200

    naming backbone

--- main/seg_classification/vit_backbone_to_details.py
+++ main/seg_classification/vit_backbone_to_details.py
@@ -10,13 +10,13 @@ VIT_SMALL_224_TRAGET_CKPT_PATH = "/raid/yuvalas/checkpoints/target/lightning_log
 
 VIT_BACKBONE_DETAILS = {
     "google/vit-base-patch16-224": {
-        "ckpt_path": {"target": VIT_BASE_224_TARGET_CKPT_PATH, "predicted": VIT_BASE_224_PREDICTED_CKPT_PATH},
+        "ckpt_path": {"target": VIT_BASE_224_TARGET_CKPT_PATH, "predicted": VIT_BASE_224_PREDICTED_CKPT_PATH, "potential_target_16_155":VIT_BASE_224_TARGET_CKPT_PATH_16_155_1000_samples},
         "img_size": 224,
         "patch_size": 16,
         "mask_loss": 50,
         "experiment_base_path": {
             "target": "/raid/yuvalas/experiments/target/direct_opt_ckpt_56_auc_15.925_model_google_vit-base-patch16-224_train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32__layers_freezed_0_by_target_gt__True",
-            "potential_target_16_55": "/raid/yuvalas/experiments/target/direct_opt_ckpt_48_auc_16.155_model_google_vit-base-patch16-224_train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32__layers_freezed_0_by_target_gt__True",
+            "potential_target_16_155": "/raid/yuvalas/experiments/target/direct_opt_ckpt_48_auc_16.155_model_google_vit-base-patch16-224_train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32__layers_freezed_0_by_target_gt__True",
             "predicted": "/raid/yuvalas/experiments/predicted/train_1000/direct_opt_ckpt_27_model_google_vit-base-patch16-224_train_uni_True_val_unif_True_activation_sigmoid__norm_by_max_p_False_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002_mlp_classifier_True__bs_32",
         },
     },
