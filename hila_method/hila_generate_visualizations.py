9635a7228dd8f0b7d2e9088aae8528b987293fae
Yuval Asher <asheryuvala@gmail.com>
2022-11-15 12:37:11 +0200

    add functions to hila visualizations

--- hila_method/hila_generate_visualizations.py
+++ hila_method/hila_generate_visualizations.py
@@ -113,8 +113,8 @@ def compute_saliency_and_save(dataloader: DataLoader) -> List[Dict[str, Tensor]]
         data = normalize(data)
         data = data.to(device)
         data.requires_grad_()
-        Res_patches = lrp.generate_LRP(data, start_layer=1, method="grad", index=None).reshape(data.shape[0], 1, 14,
-                                                                                               14)
+        Res_patches = lrp.generate_LRP(data, start_layer=1, method="grad", index=target).reshape(data.shape[0], 1, 14,
+                                                                                                 14)
         with torch.no_grad():
             Res = torch.nn.functional.interpolate(Res_patches, scale_factor=16, mode='bilinear').cpu()  # .cuda()
             Res = (Res - Res.min()) / (Res.max() - Res.min())
@@ -149,7 +149,7 @@ def compute_saliency_generator(dataloader: DataLoader):
         # ic(data.device, Res.device, Res_patches.device)
         # target = target.cpu()
         # ic(target.device)
-        yield dict(image_resized=resized_image.to(device), image_mask=Res.to(device), target=target.to(device))
+        yield dict(image_resized=resized_image.to(device), image_mask=Res.to(device), target_class=target.to(device))
         # outputs.append({'image_resized': resized_image, 'image_mask': Res, 'patches_mask': Res_patches})
 
 
@@ -184,6 +184,85 @@ def run_pic_hila(vit_for_image_classification, images_and_masks):
                                    )
 
 
+def run_perturbation_for_specific_indices(image_indices: List[int]):
+    n_samples = len(image_indices)
+    imagenet_ds = ImageNetDataset(root_dir=IMAGENET_VALIDATION_PATH, n_samples=n_samples,
+                                  list_of_images_names=image_indices, transform=transform)
+    sample_loader = torch.utils.data.DataLoader(
+        imagenet_ds,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=4
+    )
+    hila_auc_by_img_idx = {}
+    outputs = compute_saliency_and_save(dataloader=sample_loader)
+    for idx, output in enumerate(outputs):
+        auc = eval_perturbation_test(experiment_dir=Path(""), model=vit_for_classification_image, outputs=[output])
+
+        print(
+            f"hila, image: {idx}, auc_perturbation:{auc}, gt_class: {get_gt_classes(path=GT_VALIDATION_PATH_LABELS)[image_indices[idx]]}")
+
+
+def run_perturbation_for_specific_indices_and_plot_by_condition(image_indices: List[int]):
+    hila_worst_than_us_indices = []
+    HILA_AUC_WORST_THAN_IMAGE_INDICES_PATH = "/home/yuvalas/explainability/pickles/hila_image_indices_worst_than_us.pkl"
+    n_samples = len(image_indices)
+    image_indices = sorted(image_indices)
+    imagenet_ds = ImageNetDataset(root_dir=IMAGENET_VALIDATION_PATH, n_samples=n_samples,
+                                  list_of_images_names=image_indices, transform=transform)
+    sample_loader = torch.utils.data.DataLoader(
+        imagenet_ds,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=4
+    )
+    outputs = compute_saliency_generator(dataloader=sample_loader)
+    for idx, output in tqdm(enumerate(outputs)):
+        auc = eval_perturbation_test(experiment_dir=Path(""), model=vit_for_classification_image, outputs=[output])
+        if auc > 14:
+            visualized_image = plot_visualization(original_image=output["image_resized"],
+                                                  transformer_attribution=output["image_mask"])
+            hila_worst_than_us_indices.append(image_indices[idx])
+            save_image_try(image=visualized_image, image_idx=image_indices[idx])
+    print(hila_worst_than_us_indices)
+    save_obj_to_disk(path=HILA_AUC_WORST_THAN_IMAGE_INDICES_PATH, obj=hila_worst_than_us_indices)
+    # print(
+    #     f"hila, image: {idx}, auc_perturbation:{auc}, gt_class: {get_gt_classes(path=GT_VALIDATION_PATH_LABELS)[image_indices[idx]]}")
+
+
+def plot_visualization(original_image, transformer_attribution):
+    """
+    original_image.shape:
+    vis.shape: (224,224)
+    """
+    # transformer_attribution = vis.reshape(1, 1, 14, 14)
+    # transformer_attribution = torch.nn.functional.interpolate(transformer_attribution, scale_factor=16,
+    #                                                           mode='bilinear')
+    # transformer_attribution = (transformer_attribution - transformer_attribution.min()) / (
+    #         transformer_attribution.max() - transformer_attribution.min())
+    transformer_attribution = transformer_attribution.reshape(224, 224).cuda().data.cpu().numpy()
+    image_transformer_attribution = original_image.squeeze(0).permute(1, 2, 0).data.cpu().numpy()
+    image_transformer_attribution = (image_transformer_attribution - image_transformer_attribution.min()) / (
+            image_transformer_attribution.max() - image_transformer_attribution.min())
+    vis = show_cam_on_image(image_transformer_attribution, transformer_attribution)
+    vis = np.uint8(255 * vis)
+    vis = cv2.cvtColor(np.array(vis), cv2.COLOR_RGB2BGR)
+    return vis
+
+
+def save_image_try(image, image_idx: int) -> None:  # [1,3,224,224] or [3,224,224]
+    image = image if len(image.shape) == 3 else image.squeeze(0)
+    image = transforms.ToPILImage()(image)
+    image = image.resize((224, 224))
+    plt.imshow(transforms.ToTensor()(image).permute(1, 2, 0))
+    plt.axis('off');
+    # plt.show();
+    path = f"/home/yuvalas/explainability/research/plots/additional/hila1_worst_than_us/{image_idx}_hila_1.png"
+    plt.margins(x=0, y=0)
+    plt.savefig(path, dpi=300,
+                bbox_inches='tight', pad_inches=0, transparent=True)
+
+
 if __name__ == '__main__':
     BATCH_SIZE = 1
     MODEL_NAME = config["vit"]["model_name"]
@@ -193,35 +272,38 @@ if __name__ == '__main__':
 
     vit_for_classification_image, _ = load_vit_pretrained(model_name=MODEL_NAME)
     vit_for_classification_image = vit_for_classification_image.to(device)
-
+    """
+    images_indices = sorted([17721, 16167, 7436, 11616])
+    imagenet_ds = ImageNetDataset(root_dir=IMAGENET_VALIDATION_PATH, n_samples=len(images_indices),
+                                  list_of_images_names=images_indices, transform=transform)
+    sample_loader = torch.utils.data.DataLoader(
+        imagenet_ds,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=4
+    )
+    images_and_masks_generator = compute_saliency_generator(dataloader=sample_loader)
+    
+    # run_pic_hila(vit_for_image_classification=vit_for_classification_image, images_and_masks=images_and_masks_generator)
+    """
     IMAGES_IDX_BY_AUC_DIFF_BASE_OPT_MODEL_PATH = "/home/yuvalas/explainability/pickles/images_idx_by_auc_diff_base_opt_model.pkl"
     HILA_AUC_BY_IMG_IDX_PATH = "/home/yuvalas/explainability/pickles/hila_auc_by_img_idx.pkl"
     # PLOTS_OUTPUT_PATH = "/home/yuvalas/explainability/pickles/comparison_base_opt_models"
+    IMAGES_IDX_BY_AUC_TARGET_OPT_MODEL_PATH = "/home/yuvalas/explainability/pickles/images_idx_by_auc_target_opt.pkl"
+    images_idx_by_auc_target_opt = load_obj(path=IMAGES_IDX_BY_AUC_TARGET_OPT_MODEL_PATH)
 
-    images_idx_by_auc_diff_base_opt_model = load_obj(path=IMAGES_IDX_BY_AUC_DIFF_BASE_OPT_MODEL_PATH)
+    image_indices = []
+    for key in images_idx_by_auc_target_opt.keys():
+        if key in [5]:
+            image_indices.extend(images_idx_by_auc_target_opt[key])
+    image_indices = image_indices
+    print(len(image_indices))
+    n_samples = len(image_indices)
+    run_perturbation_for_specific_indices_and_plot_by_condition(image_indices=sorted(image_indices))
 
-    # images_indices = []
-    # for key in images_idx_by_auc_diff_base_opt_model.keys():
-    #     images_indices.extend(images_idx_by_auc_diff_base_opt_model[key])
-    # interesting_indices_to_look = [218, 245, 456, 457, 549, 710, 729, 754, 878, 1037, 1056, 1234, 1332, 1450, 1654,
-    #                                1671,1700, 1740, 1831, 1859, 1875, 1980, 2534, 2566, 2691, 2700, 2750, 2763, 2808,
-    #                                2820,2985,3032, 3083, 3104, 3105, 3206, 3209, 3344, 3414, 3864, 4144, 4282, 4741, 4795,
-    #                                4928,5018,5022, 5078, 5086, 5102, 5186, 5196, 5237, 5596, 5939, 6018, 6027, 6324, 6734,
-    #                                6821,6877,6912, 6984, 7021, 7268, 7306, 7341, 8048, 8268, 8311, 8408, 8600, 8693, 8734,
-    #                                8741,8971,9043, 9085, 9319, 9324, 9510, 9637, 9920, 9939, 10019, 10264, 10314, 10512,
-    #                                10830,10875,10911, 11079, 11410, 11543, 11721, 11864, 11867, 12146, 12392, 12424, 12471,
-    #                                12490,12630, 12718, 12804, 12871, 12920, 13252, 13293, 13494, 13566, 13654, 13790, 14014,
-    #                                14026, 14189, 14217, 14225, 14620, 14646, 14681, 14985, 7305, 9376, 14229, 149, 193,
-    #                                215,994, 1187, 1515, 1631, 1918, 2313, 3941, 4031, 4707, 4888, 5064, 5287, 5463,
-    #                                6045,6225,6887, 7340, 7635, 8439, 8444, 9053, 9110, 9224, 10003, 11153, 11307, 11846,
-    #                                12433,12573,14514, 14703]
-    # images_indices = interesting_indices_to_look
-
-    images_indices = list(chain(*images_idx_by_auc_diff_base_opt_model.values()))
-    print(len(images_indices))
-    n_samples = len(images_indices)
+    """
     imagenet_ds = ImageNetDataset(root_dir=IMAGENET_VALIDATION_PATH, n_samples=n_samples,
-                                  list_of_images_names=images_indices, transform=transform)
+                                  list_of_images_names=sorted(image_indices), transform=transform)
     sample_loader = torch.utils.data.DataLoader(
         imagenet_ds,
         batch_size=BATCH_SIZE,
@@ -233,11 +315,11 @@ if __name__ == '__main__':
     for idx, output in enumerate(outputs):
         auc = eval_perturbation_test(experiment_dir=Path(""), model=vit_for_classification_image, outputs=[output])
         output["auc"] = auc
-        output["image_idx"] = images_indices[idx]
+        output["image_idx"] = image_indices[idx]
         # ic(images_indices[idx])
-        hila_auc_by_img_idx[images_indices[idx]] = auc
+        hila_auc_by_img_idx[image_indices[idx]] = auc
     save_obj_to_disk(path=HILA_AUC_BY_IMG_IDX_PATH, obj=hila_auc_by_img_idx)
-
+    """
     # print(f'AUC: {round(auc, 4)}% for {len(outputs)} records')
     # for key in images_idx_by_auc_diff_base_opt_model.keys():
     #     if image_idx in images_idx_by_auc_diff_base_opt_model[key]:
