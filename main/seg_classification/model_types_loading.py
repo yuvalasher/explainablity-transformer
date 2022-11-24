95c5dff69766c35accf2cdc0fe14201f3c8b0324
Yuval Asher <asheryuvala@gmail.com>
2022-11-24 19:55:18 +0200

    export loading function to module

---
+++ main/seg_classification/model_types_loading.py
@@ -0,0 +1,67 @@
+from typing import Union
+
+from torchvision import models
+from transformers import ViTForImageClassification
+
+from feature_extractor import ViTFeatureExtractor
+from models.modeling_vit_patch_classification import ViTForMaskGeneration
+from vit_loader.load_vit import load_vit_pretrained_for_explanier, load_vit_pretrained_for_explaniee
+
+CONVNET_MODELS_BY_NAME = {"resnet": {"explaniee": models.resnet101(pretrained=True),
+                                     "explanier": None
+                                     },
+                          "densenet": {"explaniee": models.densenet201(pretrained=True),
+                                       "explanier": None
+                                       }
+                          }
+
+
+def load_vit_type_models(model_name: str, is_explanier_model: bool) -> Union[
+    ViTForImageClassification, ViTForMaskGeneration]:
+    if is_explanier_model:
+        if model_name in ["google/vit-base-patch16-224"]:
+            model_for_classification_image = load_vit_pretrained_for_explanier(model_name=model_name)
+        else:
+            model_for_classification_image = ViTForImageClassification.from_pretrained(model_name)
+        return model_for_classification_image
+    else:
+        if model_name in ["google/vit-base-patch16-224"]:
+            model_for_patch_classification = load_vit_pretrained_for_explaniee(model_name=model_name)
+        else:
+            model_for_patch_classification = ViTForMaskGeneration.from_pretrained(model_name)
+        return model_for_patch_classification
+
+
+def load_convnet_type_models(model_name: str, is_explanier_model: bool):
+    return CONVNET_MODELS_BY_NAME[model_name]["explanier" if is_explanier_model else "explaniee"]
+
+
+def load_model_by_name(model_name: str, is_explanier_model: bool):
+    if model_name in CONVNET_MODELS_BY_NAME.keys():
+        model = load_convnet_type_models(model_name=model_name, is_explanier_model=is_explanier_model)
+    else:
+        model = load_vit_type_models(model_name=model_name, is_explanier_model=is_explanier_model)
+    return model
+
+
+def load_feature_extractor(explainee_model_name: str, explainer_model_name: str) -> Union[ViTFeatureExtractor, None]:
+    """
+    If both of models are convnet, return None as feature extractor, else return feature extractor by the explanier / explaniee
+    """
+    if explainee_model_name in CONVNET_MODELS_BY_NAME.keys() and explainer_model_name in CONVNET_MODELS_BY_NAME.keys():
+        return None
+    if explainee_model_name not in CONVNET_MODELS_BY_NAME.keys():
+        return ViTFeatureExtractor.from_pretrained(explainee_model_name)
+    if explainer_model_name not in CONVNET_MODELS_BY_NAME.keys():
+        return ViTFeatureExtractor.from_pretrained(explainer_model_name)
+
+
+def load_explainer_explaniee_models_and_feature_extractor(explainee_model_name: str, explainer_model_name: str):
+    model_for_classification_image = load_model_by_name(model_name=explainee_model_name,
+                                                        is_explanier_model=False)
+    model_for_patch_classification = load_model_by_name(model_name=explainer_model_name,
+                                                        is_explanier_model=True)
+    feature_extractor = load_feature_extractor(explainee_model_name=explainee_model_name,
+                                               explainer_model_name=explainer_model_name)
+    return model_for_classification_image, model_for_patch_classification, feature_extractor
+
--- main/seg_classification/run_seg_cls.py
+++ main/seg_classification/run_seg_cls.py
@@ -1,5 +1,7 @@
 import os
 
+from main.seg_classification.model_types_loading import load_explainer_explaniee_models_and_feature_extractor
+
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
@@ -7,18 +9,12 @@ from typing import Dict, Union
 import wandb
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
-from transformers import ViTForImageClassification
-
-from feature_extractor import ViTFeatureExtractor
 from main.seg_classification.seg_cls_utils import save_config_to_root_dir
-from models.modeling_vit_patch_classification import ViTForMaskGeneration
 
 import torch
 from config import config
 from icecream import ic
 from utils import remove_old_results_dfs
-from vit_loader.load_vit import load_vit_pretrained, load_vit_pretrained_for_explanier, \
-    load_vit_pretrained_for_explaniee
 from pathlib import Path
 from main.seg_classification.image_classification_with_token_classification_model import (
     ImageClassificationWithTokenClassificationModel,
@@ -54,7 +50,6 @@ from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 gc.collect()
 
-
 batch_size = vit_config["batch_size"]
 n_epochs = vit_config["n_epochs"]
 is_sampled_train_data_uniformly = vit_config["is_sampled_train_data_uniformly"]
@@ -85,68 +80,8 @@ ic(explainee_model_name)
 
 exp_name = f'explanier_{explainer_model_name.replace("/", "_")}__explaniee_{explainee_model_name.replace("/", "_")}__train_uni_{is_sampled_train_data_uniformly}_val_unif_{is_sampled_val_data_uniformly}_activation_{vit_config["activation_function"]}_pred_{loss_multipliers["prediction_loss_mul"]}_mask_l_{loss_config["mask_loss"]}_{loss_multipliers["mask_loss_mul"]}__train_n_samples_{train_n_samples * 1000}_lr_{vit_config["lr"]}__bs_{batch_size}_by_target_gt__{train_model_by_target_gt_class}'
 
-CONVNET_MODELS_BY_NAME = {"resnet": {"explaniee": models.resnet101(pretrained=True),
-                                     "explanier": None
-                                     },
-                          "densenet": {"explaniee": models.densenet201(pretrained=True),
-                                       "explanier": None
-                                       }
-                          }
-
-
-def load_vit_type_models(model_name: str, is_explanier_model: bool) -> Union[
-    ViTForImageClassification, ViTForMaskGeneration]:
-    if is_explanier_model:
-        if model_name in ["google/vit-base-patch16-224"]:
-            model_for_classification_image = load_vit_pretrained_for_explanier(model_name=model_name)
-        else:
-            model_for_classification_image = ViTForImageClassification.from_pretrained(model_name)
-        return model_for_classification_image
-    else:
-        if model_name in ["google/vit-base-patch16-224"]:
-            model_for_patch_classification = load_vit_pretrained_for_explaniee(model_name=model_name)
-        else:
-            model_for_patch_classification = ViTForMaskGeneration.from_pretrained(model_name)
-        return model_for_patch_classification
-
-
-def load_convnet_type_models(model_name: str, is_explanier_model: bool):
-    return CONVNET_MODELS_BY_NAME[model_name]["explanier" if is_explanier_model else "explaniee"]
-
-
-def load_model_by_name(model_name: str, is_explanier_model: bool):
-    if model_name in CONVNET_MODELS_BY_NAME.keys():
-        model = load_convnet_type_models(model_name=model_name, is_explanier_model=is_explanier_model)
-    else:
-        model = load_vit_type_models(model_name=model_name, is_explanier_model=is_explanier_model)
-    return model
-
-
-def load_feature_extractor(explainee_model_name: str, explainer_model_name: str) -> Union[ViTFeatureExtractor, None]:
-    """
-    If both of models are convnet, return None as feature extractor, else return feature extractor by the explanier / explaniee
-    """
-    if explainee_model_name in CONVNET_MODELS_BY_NAME.keys() and explainer_model_name in CONVNET_MODELS_BY_NAME.keys():
-        return None
-    if explainee_model_name not in CONVNET_MODELS_BY_NAME.keys():
-        return ViTFeatureExtractor.from_pretrained(explainee_model_name)
-    if explainer_model_name not in CONVNET_MODELS_BY_NAME.keys():
-        return ViTFeatureExtractor.from_pretrained(explainer_model_name)
-
-
-def load_explainer_explaniee_models_and_feature_extractor(explainee_model_name: str, explainer_model_name: str):
-    model_for_classification_image = load_model_by_name(model_name=explainee_model_name,
-                                                        is_explanier_model=False)
-    model_for_patch_classification = load_model_by_name(model_name=explainer_model_name,
-                                                        is_explanier_model=True)
-    feature_extractor = load_feature_extractor(explainee_model_name=explainee_model_name,
-                                               explainer_model_name=explainer_model_name)
-    return model_for_classification_image, model_for_patch_classification, feature_extractor
-
-
 model_for_classification_image, model_for_patch_classification, feature_extractor = load_explainer_explaniee_models_and_feature_extractor(
     explainee_model_name=explainee_model_name, explainer_model_name=explainer_model_name)
-
 
 ic(
     str(IMAGENET_VAL_IMAGES_FOLDER_PATH),
