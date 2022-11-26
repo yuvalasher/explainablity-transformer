3c9c59ca762d1fe517992f2a0f1d9aeb94f77492
Yuval Asher <asheryuvala@gmail.com>
2022-11-26 18:02:29 +0200

    linter

--- vit_utils.py
+++ vit_utils.py
@@ -174,8 +174,9 @@ def freeze_multitask_model(model, freezing_classification_transformer: bool = Tr
         for param in model.vit_for_classification_image.parameters():
             param.requires_grad = False
 
-    modules = [model.vit_for_patch_classification.vit.embeddings, model.vit_for_patch_classification.vit.encoder.layer[
-                                                                  :segmentation_transformer_n_first_layers_to_freeze]]
+    modules = [model.vit_for_patch_classification.vit.embeddings,
+               model.vit_for_patch_classification.vit.encoder.layer[
+               :segmentation_transformer_n_first_layers_to_freeze]]
     for module in modules:
         for param in module.parameters():
             param.requires_grad = False
