9fd8c6097191f7d76886b4b6da056a8d5b627670
yuvalasher <asheryuvala@gmail.com>
2022-12-03 00:34:43 +0200

    run_seg_cls_opt.py to argparse

--- main/seg_classification/run_seg_cls_opt.py
+++ main/seg_classification/run_seg_cls_opt.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -20,7 +21,7 @@ import pytorch_lightning as pl
 from utils.consts import (
     IMAGENET_VAL_IMAGES_FOLDER_PATH,
     EXPERIMENTS_FOLDER_PATH, RESULTS_PICKLES_FOLDER_PATH,
-    GT_VALIDATION_PATH_LABELS,
+    GT_VALIDATION_PATH_LABELS, MODEL_OPTIONS, MODEL_ALIAS_MAPPING,
 )
 from main.seg_classification.backbone_to_details import BACKBONE_DETAILS
 from vit_utils import (
@@ -41,107 +42,142 @@ logging.getLogger('lightning').setLevel(0)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
-batch_size, n_epochs, is_sampled_train_data_uniformly, is_sampled_val_data_uniformly, \
-train_model_by_target_gt_class, is_freezing_explaniee_model, \
-explainer_model_n_first_layers_to_freeze, is_clamp_between_0_to_1, enable_checkpointing, \
-is_competitive_method_transforms, explainer_model_name, explainee_model_name, plot_path, default_root_dir, \
-mask_loss, mask_loss_mul, prediction_loss_mul, lr, start_epoch_to_evaluate, \
-n_batches_to_visualize, is_ce_neg, activation_function, n_epochs_to_optimize_stage_b, RUN_BASE_MODEL, \
-use_logits_only, VERBOSE, IMG_SIZE, PATCH_SIZE, evaluation_experiment_folder_name, train_n_label_sample, \
-val_n_label_sample = get_params_from_config(config_vit=config["vit"])
-
-IS_EXPLANIEE_CONVNET = True if explainee_model_name in CONVNET_MODELS_BY_NAME.keys() else False
-IS_EXPLAINER_CONVNET = True if explainer_model_name in CONVNET_MODELS_BY_NAME.keys() else False
-
-seed_everything(config["general"]["seed"])
-enable_checkpointing = False
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-gc.collect()
-
-get_loss_multipliers(normalize=False,
-                     mask_loss_mul=mask_loss_mul,
-                     prediction_loss_mul=prediction_loss_mul)
-target_or_predicted_model = "target" if train_model_by_target_gt_class else "predicted"
-
-CKPT_PATH, IMG_SIZE, PATCH_SIZE, MASK_LOSS_MUL = BACKBONE_DETAILS[explainee_model_name]["ckpt_path"][
-                                                     target_or_predicted_model], \
-                                                 BACKBONE_DETAILS[explainee_model_name]["img_size"], \
-                                                 BACKBONE_DETAILS[explainee_model_name]["patch_size"], \
-                                                 BACKBONE_DETAILS[explainee_model_name]["mask_loss"]
-mask_loss_mul = MASK_LOSS_MUL
-CHECKPOINT_EPOCH_IDX = get_checkpoint_idx(ckpt_path=CKPT_PATH)
-BASE_CKPT_MODEL_AUC = get_ckpt_model_auc(ckpt_path=CKPT_PATH)
-
-exp_name = f'direct_opt_ckpt_{CHECKPOINT_EPOCH_IDX}_auc_{BASE_CKPT_MODEL_AUC}_explanier_{explainer_model_name.replace("/", "_")}__explaniee_{explainee_model_name.replace("/", "_")}__train_uni_{is_sampled_train_data_uniformly}_val_unif_{is_sampled_val_data_uniformly}_activation_{activation_function}_pred_{prediction_loss_mul}_mask_l_{mask_loss}_{mask_loss_mul}__train_n_samples_{train_n_label_sample * 1000}_lr_{lr}__bs_{batch_size}_by_target_gt__{train_model_by_target_gt_class}'
-plot_path = Path(plot_path, exp_name)
-BASE_AUC_OBJECTS_PATH = Path(RESULTS_PICKLES_FOLDER_PATH, 'target' if train_model_by_target_gt_class else 'predicted')
-
-EXP_PATH = Path(BASE_AUC_OBJECTS_PATH, exp_name)
-os.makedirs(EXP_PATH, exist_ok=True)
-ic(VERBOSE)
-ic(EXP_PATH, RUN_BASE_MODEL)
-ic(mask_loss_mul)
-ic(train_model_by_target_gt_class)
-ic(str(IMAGENET_VAL_IMAGES_FOLDER_PATH))
-
-BEST_AUC_PLOT_PATH, BEST_AUC_OBJECTS_PATH, BASE_MODEL_BEST_AUC_PLOT_PATH, BASE_MODEL_BEST_AUC_OBJECTS_PATH = create_folder_hierarchy(
-    base_auc_objects_path=BASE_AUC_OBJECTS_PATH, exp_name=exp_name)
-
-model_for_classification_image, model_for_mask_generation, feature_extractor = load_explainer_explaniee_models_and_feature_extractor(
-    explainee_model_name=explainee_model_name,
-    explainer_model_name=explainer_model_name,
-    activation_function=activation_function,
-    img_size=IMG_SIZE,
-)
-
-warmup_steps, total_training_steps = get_warmup_steps_and_total_training_steps(
-    n_epochs=n_epochs_to_optimize_stage_b,
-    train_samples_length=len(list(Path(IMAGENET_VAL_IMAGES_FOLDER_PATH).iterdir())),
-    batch_size=batch_size,
-)
-
-model = OptImageClassificationWithTokenClassificationModel(
-    model_for_classification_image=model_for_classification_image,
-    model_for_mask_generation=model_for_mask_generation,
-    is_clamp_between_0_to_1=is_clamp_between_0_to_1,
-    plot_path=plot_path,
-    warmup_steps=warmup_steps,
-    total_training_steps=total_training_steps,
-    best_auc_objects_path=BASE_MODEL_BEST_AUC_OBJECTS_PATH if RUN_BASE_MODEL else BEST_AUC_OBJECTS_PATH,
-    checkpoint_epoch_idx=CHECKPOINT_EPOCH_IDX,
-    best_auc_plot_path=BASE_MODEL_BEST_AUC_PLOT_PATH if RUN_BASE_MODEL else BEST_AUC_PLOT_PATH,
-    run_base_model_only=RUN_BASE_MODEL,
-    is_explainee_convnet=IS_EXPLANIEE_CONVNET,
-    is_explainer_convnet=IS_EXPLAINER_CONVNET,
-    lr=lr,
-    n_epochs=n_epochs,
-    start_epoch_to_evaluate=start_epoch_to_evaluate,
-    train_model_by_target_gt_class=train_model_by_target_gt_class,
-    use_logits_only=use_logits_only,
-    n_batches_to_visualize=n_batches_to_visualize,
-    activation_function=activation_function,
-    img_size=IMG_SIZE,
-    patch_size=PATCH_SIZE,
-    is_ce_neg=is_ce_neg,
-    verbose=VERBOSE,
-)
-
-experiment_path = Path(EXPERIMENTS_FOLDER_PATH, evaluation_experiment_folder_name)
-remove_old_results_dfs(experiment_path=experiment_path)
-model = freeze_multitask_model(
-    model=model,
-    is_freezing_explaniee_model=is_freezing_explaniee_model,
-    explainer_model_n_first_layers_to_freeze=explainer_model_n_first_layers_to_freeze,
-    is_explainer_convnet=IS_EXPLAINER_CONVNET,
-)
-print(exp_name)
-print_number_of_trainable_and_not_trainable_params(model)
-
 if __name__ == '__main__':
+    params_config = get_params_from_config(config_vit=config["vit"])
+    parser = argparse.ArgumentParser(description='Fine-tune LTX model')
+    parser.add_argument('--explainer-model-name', type=str, default="vit_224_base", choices=MODEL_OPTIONS)
+    parser.add_argument('--explainee-model-name', type=str, default="vit_224_base", choices=MODEL_OPTIONS)
+    parser.add_argument('--train-model-by-target-gt-class', type=bool,
+                        default=params_config["train_model_by_target_gt_class"])
+    parser.add_argument('--RUN-BASE-MODEL', type=bool, default=params_config["RUN_BASE_MODEL"])
+    parser.add_argument('--enable-checkpointing', type=bool, default=False)
+
+    parser.add_argument('--mask-loss-mul', type=int, default=params_config["mask_loss_mul"])
+    parser.add_argument('--prediction-loss-mul', type=int, default=params_config["prediction_loss_mul"])
+    parser.add_argument('--n-epochs', type=int, default=params_config["n_epochs"])
+    parser.add_argument('--n_epochs_to_optimize_stage_b', type=int, default=params_config["n_epochs"])
+    parser.add_argument('--batch-size', type=int, default=1)
+    parser.add_argument('--verbose', type=bool, default=params_config["verbose"])
+
+    parser.add_argument('--is-freezing-explaniee-model', type=bool,
+                        default=params_config["is_freezing_explaniee_model"])
+    parser.add_argument('--explainer-model-n-first-layers-to-freeze', type=int,
+                        default=params_config["explainer_model_n_first_layers_to_freeze"])
+    parser.add_argument('--is-clamp-between-0-to-1', type=bool, default=params_config["is_clamp_between_0_to_1"])
+    parser.add_argument('--is-competitive-method-transforms', type=bool,
+                        default=params_config["is_competitive_method_transforms"])
+    parser.add_argument('--plot-path', type=str, default=params_config["plot_path"])
+    parser.add_argument('--default-root-dir', type=str, default=params_config["default_root_dir"])
+    parser.add_argument('--mask-loss', type=str, default=params_config["mask_loss"])
+    parser.add_argument('--lr', type=float, default=params_config["lr"])
+    parser.add_argument('--start-epoch-to-evaluate', type=int, default=params_config["start_epoch_to_evaluate"])
+    parser.add_argument('--n-batches-to-visualize', type=int, default=params_config["n_batches_to_visualize"])
+    parser.add_argument('--is-ce-neg', type=str, default=params_config["is_ce_neg"])
+    parser.add_argument('--activation-function', type=str, default=params_config["activation_function"])
+    parser.add_argument('--use-logits-only', type=bool, default=params_config["use_logits_only"])
+    parser.add_argument('--img-size', type=int, default=params_config["img_size"])
+    parser.add_argument('--patch-size', type=int, default=params_config["patch_size"])
+    parser.add_argument('--evaluation-experiment-folder-name', type=str,
+                        default=params_config["evaluation_experiment_folder_name"])
+
+    args = parser.parse_args()
+    EXPLAINEE_MODEL_NAME, EXPLAINER_MODEL_NAME = MODEL_ALIAS_MAPPING[args.explainee_model_name], \
+                                                 MODEL_ALIAS_MAPPING[args.explainer_model_name]
+
+    IS_EXPLANIEE_CONVNET = True if EXPLAINEE_MODEL_NAME in CONVNET_MODELS_BY_NAME.keys() else False
+    IS_EXPLAINER_CONVNET = True if EXPLAINER_MODEL_NAME in CONVNET_MODELS_BY_NAME.keys() else False
+
+    seed_everything(config["general"]["seed"])
+    enable_checkpointing = False
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    gc.collect()
+
+    get_loss_multipliers(normalize=False,
+                         mask_loss_mul=args.mask_loss_mul,
+                         prediction_loss_mul=args.prediction_loss_mul)
+    target_or_predicted_model = "target" if args.train_model_by_target_gt_class else "predicted"
+    # TODO - need to re-design the BACKBONE_DETAILS
+    CKPT_PATH, IMG_SIZE, PATCH_SIZE, MASK_LOSS_MUL = BACKBONE_DETAILS[EXPLAINEE_MODEL_NAME]["ckpt_path"][
+                                                         target_or_predicted_model], \
+                                                     BACKBONE_DETAILS[EXPLAINEE_MODEL_NAME]["img_size"], \
+                                                     BACKBONE_DETAILS[EXPLAINEE_MODEL_NAME]["patch_size"], \
+                                                     BACKBONE_DETAILS[EXPLAINEE_MODEL_NAME]["mask_loss"]
+    args.mask_loss_mul = MASK_LOSS_MUL
+    args.img_size = IMG_SIZE
+    args.patch_size = PATCH_SIZE
+    CHECKPOINT_EPOCH_IDX = get_checkpoint_idx(ckpt_path=CKPT_PATH)
+    BASE_CKPT_MODEL_AUC = get_ckpt_model_auc(ckpt_path=CKPT_PATH)
+
+    exp_name = f'direct_opt_ckpt_{CHECKPOINT_EPOCH_IDX}_auc_{BASE_CKPT_MODEL_AUC}_explanier_{args.explainer_model_name.replace("/", "_")}__explaniee_{args.explainee_model_name.replace("/", "_")}__train_uni_{args.is_sampled_train_data_uniformly}_val_unif_{args.is_sampled_val_data_uniformly}_activation_{args.activation_function}_pred_{args.prediction_loss_mul}_mask_l_{args.mask_loss}_{args.mask_loss_mul}__train_n_samples_{args.train_n_label_sample * 1000}_lr_{args.lr}__bs_{args.batch_size}_by_target_gt__{args.train_model_by_target_gt_class}'
+    plot_path = Path(args.plot_path, exp_name)
+    BASE_AUC_OBJECTS_PATH = Path(RESULTS_PICKLES_FOLDER_PATH, 'target' if args.train_model_by_target_gt_class else 'predicted')
+
+    EXP_PATH = Path(BASE_AUC_OBJECTS_PATH, exp_name)
+    os.makedirs(EXP_PATH, exist_ok=True)
+    ic(args.verbose)
+    ic(EXP_PATH)
+    ic(args.RUN_BASE_MODEL)
+    ic(args.mask_loss_mul)
+    ic(args.train_model_by_target_gt_class)
+    ic(str(IMAGENET_VAL_IMAGES_FOLDER_PATH))
+
+    BEST_AUC_PLOT_PATH, BEST_AUC_OBJECTS_PATH, BASE_MODEL_BEST_AUC_PLOT_PATH, BASE_MODEL_BEST_AUC_OBJECTS_PATH = create_folder_hierarchy(
+        base_auc_objects_path=BASE_AUC_OBJECTS_PATH, exp_name=exp_name)
+
+    model_for_classification_image, model_for_mask_generation, feature_extractor = load_explainer_explaniee_models_and_feature_extractor(
+        explainee_model_name=EXPLAINEE_MODEL_NAME,
+        explainer_model_name=EXPLAINER_MODEL_NAME,
+        activation_function=args.activation_function,
+        img_size=args.img_size,
+    )
+
+    warmup_steps, total_training_steps = get_warmup_steps_and_total_training_steps(
+        n_epochs=args.n_epochs_to_optimize_stage_b,
+        train_samples_length=len(list(Path(IMAGENET_VAL_IMAGES_FOLDER_PATH).iterdir())),
+        batch_size=args.batch_size,
+    )
+
+    model = OptImageClassificationWithTokenClassificationModel(
+        model_for_classification_image=model_for_classification_image,
+        model_for_mask_generation=model_for_mask_generation,
+        is_clamp_between_0_to_1=args.is_clamp_between_0_to_1,
+        plot_path=plot_path,
+        warmup_steps=warmup_steps,
+        total_training_steps=total_training_steps,
+        best_auc_objects_path=BASE_MODEL_BEST_AUC_OBJECTS_PATH if args.RUN_BASE_MODEL else BEST_AUC_OBJECTS_PATH,
+        checkpoint_epoch_idx=CHECKPOINT_EPOCH_IDX,
+        best_auc_plot_path=BASE_MODEL_BEST_AUC_PLOT_PATH if args.RUN_BASE_MODEL else BEST_AUC_PLOT_PATH,
+        run_base_model_only=args.RUN_BASE_MODEL,
+        is_explainee_convnet=IS_EXPLANIEE_CONVNET,
+        is_explainer_convnet=IS_EXPLAINER_CONVNET,
+        lr=args.lr,
+        n_epochs=args.n_epochs,
+        start_epoch_to_evaluate=args.start_epoch_to_evaluate,
+        train_model_by_target_gt_class=args.train_model_by_target_gt_class,
+        use_logits_only=args.use_logits_only,
+        n_batches_to_visualize=args.n_batches_to_visualize,
+        activation_function=args.activation_function,
+        img_size=args.img_size,
+        patch_size=args.patch_size,
+        is_ce_neg=args.is_ce_neg,
+        verbose=args.verbose,
+    )
+
+    experiment_path = Path(EXPERIMENTS_FOLDER_PATH, args.evaluation_experiment_folder_name)
+    remove_old_results_dfs(experiment_path=experiment_path)
+    model = freeze_multitask_model(
+        model=model,
+        is_freezing_explaniee_model=args.is_freezing_explaniee_model,
+        explainer_model_n_first_layers_to_freeze=args.explainer_model_n_first_layers_to_freeze,
+        is_explainer_convnet=IS_EXPLAINER_CONVNET,
+    )
+    print(exp_name)
+    print_number_of_trainable_and_not_trainable_params(model)
+
     IMAGES_PATH = IMAGENET_VAL_IMAGES_FOLDER_PATH
     ic(exp_name)
     print(f"Total Images in path: {len(os.listdir(IMAGES_PATH))}")
-    ic(mask_loss_mul, prediction_loss_mul)
+    ic(args.mask_loss_mul, args.prediction_loss_mul)
     listdir = sorted(list(Path(IMAGES_PATH).iterdir()))
     targets = get_gt_classes(path=GT_VALIDATION_PATH_LABELS)
     for idx, (image_path, target) in tqdm(enumerate(zip(listdir, targets)), position=0, leave=True, total=len(listdir)):
@@ -150,7 +186,7 @@ if __name__ == '__main__':
             train_image_path=str(image_path),
             val_image_path=str(image_path),
             target=target,
-            is_competitive_method_transforms=is_competitive_method_transforms,
+            is_competitive_method_transforms=args.is_competitive_method_transforms,
             feature_extractor=feature_extractor,
             is_explaniee_convnet=IS_EXPLANIEE_CONVNET,
         )
@@ -161,15 +197,15 @@ if __name__ == '__main__':
             devices=[1, 2],
             num_sanity_val_steps=0,
             check_val_every_n_epoch=100,
-            max_epochs=CHECKPOINT_EPOCH_IDX + n_epochs_to_optimize_stage_b,
+            max_epochs=CHECKPOINT_EPOCH_IDX + args.n_epochs_to_optimize_stage_b,
             resume_from_checkpoint=CKPT_PATH,
             enable_progress_bar=False,
             enable_checkpointing=False,
-            default_root_dir=default_root_dir,
+            default_root_dir=args.default_root_dir,
             weights_summary=None
         )
         trainer.fit(model=model, datamodule=data_module)
     mean_auc = load_pickles_and_calculate_auc(
-        path=BASE_MODEL_BEST_AUC_OBJECTS_PATH if RUN_BASE_MODEL else BEST_AUC_OBJECTS_PATH)
+        path=BASE_MODEL_BEST_AUC_OBJECTS_PATH if args.RUN_BASE_MODEL else BEST_AUC_OBJECTS_PATH)
     print(f"Mean AUC: {mean_auc}")
     ic(exp_name)
