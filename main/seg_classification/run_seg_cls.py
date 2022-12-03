4191c0f39ce6040e848c9dd2f0a71162254d56bf
yuvalasher <asheryuvala@gmail.com>
2022-12-03 14:13:52 +0200

    update example running command in run_seg_cls.py

--- main/seg_classification/run_seg_cls.py
+++ main/seg_classification/run_seg_cls.py
@@ -43,7 +43,7 @@ gc.collect()
 
 if __name__ == '__main__':
     """
-    CUDA_VISIBLE_DEVICES=1 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --explainer-model-name resnet --explainee-model-name resnet --train-model-by-target-gt-class True &> nohups_logs/test_argparse.out &
+    CUDA_VISIBLE_DEVICES=1 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --explainer-model-name resnet --explainee-model-name resnet --mask-loss-mul 50 --train-model-by-target-gt-class True &> nohups_logs/cnn/explainer_resnet_explaniee_resnet_bs32_ml_50.out &
     """
     params_config = get_params_from_config(config_vit=config["vit"])
 
