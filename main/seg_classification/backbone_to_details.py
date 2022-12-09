968ab4898940c95531cbe1655c28be216ac0bc1a
yuvalasher <asheryuvala@gmail.com>
2022-12-09 16:51:47 +0200

    update backbones ckpt paths

--- main/seg_classification/backbone_to_details.py
+++ main/seg_classification/backbone_to_details.py
@@ -11,11 +11,15 @@ VIT_SMALL_224_VIT_SMALL_224_PREDICTED_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "v
                                                        "pLTE_vit_small_224_predicted_best_auc__epoch=3_val_epoch_auc=0.ckpt")
 VIT_SMALL_224_VIT_SMALL_224_TRAGET_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_small",
                                                     "pLTE_vit_small_224_target_best_auc__epoch=11_val_epoch_auc=0.ckpt")
-RESNET_RESNET_TARGET_CKPT_PATH = Path("")
-RESNET_RESNET_PREDICTED_CKPT_PATH = Path("")
+RESNET_RESNET_TARGET_CKPT_PATH = Path(
+    "/raid/yuvalas/checkpoints/target/ARGPARSE_explanier_resnet__explaniee_resnet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__True/epoch=251_val/epoch_auc=18.475.ckpt")
+RESNET_RESNET_PREDICTED_CKPT_PATH_200__18_95 = Path(
+    "/raid/yuvalas/checkpoints/predicted/ARGPARSE_explanier_resnet__explaniee_resnet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__False/epoch=204_val/epoch_auc=18.950.ckpt")
 
-DENSENET_DENSENET_TARGET_CKPT_PATH = Path("")
-DENSENET_DENSENET_PREDICTED_CKPT_PATH = Path("")
+DENSENET_DENSENET_TARGET_CKPT_PATH = Path(
+    "/raid/yuvalas/checkpoints/target/ARGPARSE_explanier_densenet__explaniee_densenet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__True/epoch=286_val/epoch_auc=18.565.ckpt")
+DENSENET_DENSENET_PREDICTED_CKPT_PATH_194__18_655 = Path(
+    "/raid/yuvalas/checkpoints/predicted/ARGPARSE_explanier_densenet__explaniee_densenet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__False/epoch=194_val/epoch_auc=18.655.ckpt")
 
 BACKBONE_DETAILS = {
     "google/vit-base-patch16-224": {
@@ -39,28 +43,7 @@ BACKBONE_DETAILS = {
             "target": "/raid/yuvalas/experiments/target/direct_opt_ckpt_12_auc_14.855_model_WinKawaks_vit-small-patch16-224_train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_30__train_n_samples_1000_lr_0.002__bs_32__layers_freezed_0_by_target_gt__True",
             "predicted": "/raid/yuvalas/experiments/predicted/direct_opt_ckpt_4_auc_16.95_model_WinKawaks_vit-small-patch16-224_train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_30__train_n_samples_1000_lr_0.002__bs_32__layers_freezed_0_by_target_gt__False",
         },
-    },
-    "densenet": {
-        "ckpt_path": {"target": None, "predicted": None},
-        "img_size": 224,
-        "patch_size": 16,
-        "mask_loss": 50,
-        "experiment_base_path": {
-            "target": None,
-            "predicted": None,
-        },
-    },
-    "resnet": {
-        "ckpt_path": {"target": None, "predicted": None},
-        "img_size": 224,
-        "patch_size": 16,
-        "mask_loss": 50,
-        "experiment_base_path": {
-            "target": None,
-            "predicted": None,
-        },
     }
-
 }
 
 EXPLAINER_EXPLAINEE_BACKBONE_DETAILS = {  # key: explainer_name-explainee_name
@@ -91,7 +74,8 @@ EXPLAINER_EXPLAINEE_BACKBONE_DETAILS = {  # key: explainer_name-explainee_name
         },
     },
     "densenet-densenet": {
-        "ckpt_path": {"target": None, "predicted": None},
+        "ckpt_path": {"target": DENSENET_DENSENET_TARGET_CKPT_PATH,
+                      "predicted": DENSENET_DENSENET_PREDICTED_CKPT_PATH_234__18_86},
         "img_size": 224,
         "patch_size": None,
         "mask_loss": 50,
@@ -103,7 +87,8 @@ EXPLAINER_EXPLAINEE_BACKBONE_DETAILS = {  # key: explainer_name-explainee_name
         },
     },
     "resnet-resnet": {
-        "ckpt_path": {"target": None, "predicted": None},
+        "ckpt_path": {"target": RESNET_RESNET_TARGET_CKPT_PATH,
+                      "predicted": RESNET_RESNET_PREDICTED_CKPT_PATH_200__18_95},
         "img_size": 224,
         "patch_size": None,
         "mask_loss": 50,
