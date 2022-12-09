ee320d3286602d46cc3480f1cdd21d72a1ebe04b
yuvalasher <asheryuvala@gmail.com>
2022-12-09 15:15:02 +0200

    naming backbones to explainer-explainee convention

--- main/seg_classification/backbone_to_details.py
+++ main/seg_classification/backbone_to_details.py
@@ -2,22 +2,23 @@ from pathlib import Path
 
 from utils.consts import PLTE_CHECKPOINTS_PATH
 
-VIT_BASE_224_PREDICTED_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_base",
-                                        "pLTE_vit_base_224_predicted_best_auc__epoch=27_val_epoch_auc=0.ckpt")
-VIT_BASE_224_TARGET_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_base",
-                                     "pLTE_vit_base_224_target_best_auc__epoch=55_val_epoch_auc=0.ckpt")
+VIT_BASE_224_VIT_BASE_224_PREDICTED_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_base",
+                                                     "pLTE_vit_base_224_predicted_best_auc__epoch=27_val_epoch_auc=0.ckpt")
+VIT_BASE_224_VIT_BASE_224_TARGET_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_base",
+                                                  "pLTE_vit_base_224_target_best_auc__epoch=55_val_epoch_auc=0.ckpt")
 
-VIT_SMALL_224_PREDICTED_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_small",
-                                         "pLTE_vit_small_224_predicted_best_auc__epoch=3_val_epoch_auc=0.ckpt")
-VIT_SMALL_224_TRAGET_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_small",
-                                      "pLTE_vit_small_224_target_best_auc__epoch=11_val_epoch_auc=0.ckpt")
+VIT_SMALL_224_VIT_SMALL_224_PREDICTED_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_small",
+                                                       "pLTE_vit_small_224_predicted_best_auc__epoch=3_val_epoch_auc=0.ckpt")
+VIT_SMALL_224_VIT_SMALL_224_TRAGET_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_small",
+                                                    "pLTE_vit_small_224_target_best_auc__epoch=11_val_epoch_auc=0.ckpt")
 
 DENSENET_EXPLAINEE_TARGET_CKPT_PATH = Path("")
 RESNET_EXPLAINEE_TARGET_CKPT_PATH = Path("")
 
 BACKBONE_DETAILS = {
     "google/vit-base-patch16-224": {
-        "ckpt_path": {"target": VIT_BASE_224_TARGET_CKPT_PATH, "predicted": VIT_BASE_224_PREDICTED_CKPT_PATH},
+        "ckpt_path": {"target": VIT_BASE_224_VIT_BASE_224_TARGET_CKPT_PATH,
+                      "predicted": VIT_BASE_224_VIT_BASE_224_PREDICTED_CKPT_PATH},
         "img_size": 224,
         "patch_size": 16,
         "mask_loss": 50,
@@ -27,7 +28,8 @@ BACKBONE_DETAILS = {
         },
     },
     "WinKawaks/vit-small-patch16-224": {
-        "ckpt_path": {"target": VIT_SMALL_224_TRAGET_CKPT_PATH, "predicted": VIT_SMALL_224_PREDICTED_CKPT_PATH},
+        "ckpt_path": {"target": VIT_SMALL_224_VIT_SMALL_224_TRAGET_CKPT_PATH,
+                      "predicted": VIT_SMALL_224_VIT_SMALL_224_PREDICTED_CKPT_PATH},
         "img_size": 224,
         "patch_size": 16,
         "mask_loss": 30,
@@ -61,7 +63,8 @@ BACKBONE_DETAILS = {
 
 EXPLAINER_EXPLAINEE_BACKBONE_DETAILS = {  # key: explainer_name-explainee_name
     "vit_base_224-vit_base_224": {
-        "ckpt_path": {"target": VIT_BASE_224_TARGET_CKPT_PATH, "predicted": VIT_BASE_224_PREDICTED_CKPT_PATH},
+        "ckpt_path": {"target": VIT_BASE_224_VIT_BASE_224_TARGET_CKPT_PATH,
+                      "predicted": VIT_BASE_224_VIT_BASE_224_PREDICTED_CKPT_PATH},
         "img_size": 224,
         "patch_size": 16,
         "mask_loss": 50,
@@ -73,7 +76,8 @@ EXPLAINER_EXPLAINEE_BACKBONE_DETAILS = {  # key: explainer_name-explainee_name
         },
     },
     "vit_small_224-vit_small_224": {
-        "ckpt_path": {"target": VIT_SMALL_224_TRAGET_CKPT_PATH, "predicted": VIT_SMALL_224_PREDICTED_CKPT_PATH},
+        "ckpt_path": {"target": VIT_SMALL_224_VIT_SMALL_224_TRAGET_CKPT_PATH,
+                      "predicted": VIT_SMALL_224_VIT_SMALL_224_PREDICTED_CKPT_PATH},
         "img_size": 224,
         "patch_size": 16,
         "mask_loss": 30,
