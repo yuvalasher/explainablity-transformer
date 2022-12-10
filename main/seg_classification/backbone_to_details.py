05b0a998070a15236571bd9b461eaf1859419b76
yuvalasher <asheryuvala@gmail.com>
2022-12-10 19:16:05 +0200

    update ckpt paths and remove original BACKBONE_DETAILS

--- main/seg_classification/backbone_to_details.py
+++ main/seg_classification/backbone_to_details.py
@@ -12,39 +12,15 @@ VIT_SMALL_224_VIT_SMALL_224_PREDICTED_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "v
 VIT_SMALL_224_VIT_SMALL_224_TRAGET_CKPT_PATH = Path(PLTE_CHECKPOINTS_PATH, "vit_small",
                                                     "pLTE_vit_small_224_target_best_auc__epoch=11_val_epoch_auc=0.ckpt")
 RESNET_RESNET_TARGET_CKPT_PATH = Path(
-    "/raid/yuvalas/checkpoints/target/ARGPARSE_explanier_resnet__explaniee_resnet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__True/epoch=251_val/epoch_auc=18.475.ckpt")
+    "/raid/yuvalas/checkpoints/target/ARGPARSE_explanier_resnet__explaniee_resnet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__True/epoch=85_val/epoch_auc=19.145.ckpt")
 RESNET_RESNET_PREDICTED_CKPT_PATH_93__19_1 = Path(
     "/raid/yuvalas/checkpoints/predicted/ARGPARSE_explanier_resnet__explaniee_resnet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__False/epoch=93_val/epoch_auc=19.100.ckpt")
 
 DENSENET_DENSENET_TARGET_CKPT_PATH = Path(
-    "/raid/yuvalas/checkpoints/target/ARGPARSE_explanier_densenet__explaniee_densenet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__True/epoch=286_val/epoch_auc=18.565.ckpt")
+    "/raid/yuvalas/checkpoints/target/ARGPARSE_explanier_densenet__explaniee_densenet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__True/epoch=200_val/epoch_auc=18.855.ckpt")
 DENSENET_DENSENET_PREDICTED_CKPT_PATH_192 = Path(
     "/raid/yuvalas/checkpoints/predicted/ARGPARSE_explanier_densenet__explaniee_densenet__train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32_by_target_gt__False/epoch=192_val/epoch_auc=18.875.ckpt")
 
-BACKBONE_DETAILS = {
-    "google/vit-base-patch16-224": {
-        "ckpt_path": {"target": VIT_BASE_224_VIT_BASE_224_TARGET_CKPT_PATH,
-                      "predicted": VIT_BASE_224_VIT_BASE_224_PREDICTED_CKPT_PATH},
-        "img_size": 224,
-        "patch_size": 16,
-        "mask_loss": 50,
-        "experiment_base_path": {
-            "target": "/raid/yuvalas/experiments/target/direct_opt_ckpt_56_auc_15.925_model_google_vit-base-patch16-224_train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002__bs_32__layers_freezed_0_by_target_gt__True",
-            "predicted": "/raid/yuvalas/experiments/predicted/train_1000/direct_opt_ckpt_27_model_google_vit-base-patch16-224_train_uni_True_val_unif_True_activation_sigmoid__norm_by_max_p_False_pred_1_mask_l_bce_50__train_n_samples_1000_lr_0.002_mlp_classifier_True__bs_32",
-        },
-    },
-    "WinKawaks/vit-small-patch16-224": {
-        "ckpt_path": {"target": VIT_SMALL_224_VIT_SMALL_224_TRAGET_CKPT_PATH,
-                      "predicted": VIT_SMALL_224_VIT_SMALL_224_PREDICTED_CKPT_PATH},
-        "img_size": 224,
-        "patch_size": 16,
-        "mask_loss": 30,
-        "experiment_base_path": {
-            "target": "/raid/yuvalas/experiments/target/direct_opt_ckpt_12_auc_14.855_model_WinKawaks_vit-small-patch16-224_train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_30__train_n_samples_1000_lr_0.002__bs_32__layers_freezed_0_by_target_gt__True",
-            "predicted": "/raid/yuvalas/experiments/predicted/direct_opt_ckpt_4_auc_16.95_model_WinKawaks_vit-small-patch16-224_train_uni_True_val_unif_True_activation_sigmoid_pred_1_mask_l_bce_30__train_n_samples_1000_lr_0.002__bs_32__layers_freezed_0_by_target_gt__False",
-        },
-    }
-}
 
 EXPLAINER_EXPLAINEE_BACKBONE_DETAILS = {  # key: explainer_name-explainee_name
     "vit_base_224-vit_base_224": {
