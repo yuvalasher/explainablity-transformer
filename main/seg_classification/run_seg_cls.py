abe55621154e27b0bac2971a3c0f89ca69aabf5f
yuvalasher <asheryuvala@gmail.com>
2022-12-25 00:56:00 +0200

    update running examples

--- main/seg_classification/run_seg_cls.py
+++ main/seg_classification/run_seg_cls.py
@@ -45,14 +45,8 @@ gc.collect()
 
 if __name__ == '__main__':
     """
-    V - CUDA_VISIBLE_DEVICES=1 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --enable-checkpointing True --explainer-model-name vit_base_224 --explainee-model-name resnet --mask-loss-mul 50 --train-model-by-target-gt-class True --n-epochs 200 &> nohups_logs/journal/explainer_vit_base_explaniee_resnet_bs32_ml50.out &
-    CUDA_VISIBLE_DEVICES=1 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --enable-checkpointing True --explainer-model-name vit_base_224 --explainee-model-name resnet --mask-loss-mul 50 --train-model-by-target-gt-class False --n-epochs 200 &> nohups_logs/journal/explainer_vit_base_explaniee_resnet_bs32_ml50_predicted.out &
-    
-    V - CUDA_VISIBLE_DEVICES=1 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --enable-checkpointing True --explainer-model-name resnet --explainee-model-name vit_base_224 --mask-loss-mul 50 --train-model-by-target-gt-class True --n-epochs 200 &> nohups_logs/journal/explainer_resnet_explaniee_vit_base_bs32_ml50.out &
-    CUDA_VISIBLE_DEVICES=1 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --enable-checkpointing True --explainer-model-name resnet --explainee-model-name vit_base_224 --mask-loss-mul 50 --train-model-by-target-gt-class False --n-epochs 200 &> nohups_logs/journal/explainer_resnet_explaniee_vit_base_bs32_ml50_predicted.out &
-    
-    CUDA_VISIBLE_DEVICES=3 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --enable-checkpointing True --explainer-model-name resnet --explainee-model-name resnet --mask-loss-mul 50 --train-model-by-target-gt-class True --n-epochs 300 &> nohups_logs/journal/explainer_resnet_explaniee_resnet_bs32_ml50_target.out &
-    CUDA_VISIBLE_DEVICES=2 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --enable-checkpointing True --explainer-model-name resnet --explainee-model-name resnet --mask-loss-mul 50 --train-model-by-target-gt-class False --n-epochs 300 &> nohups_logs/journal/explainer_resnet_explaniee_resnet_bs32_ml50_predicted.out &
+    CUDA_VISIBLE_DEVICES=0 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --enable-checkpointing True --explainer-model-name resnet --explainee-model-name resnet --mask-loss-mul 50 --train-model-by-target-gt-class True --n-epochs 30 --train-n-label-sample 6 &> nohups_logs/journal/resnet_6000_bs32_ml50_target.out &
+    CUDA_VISIBLE_DEVICES=0 PYTHONPATH=./:$PYTHONPATH nohup python main/seg_classification/run_seg_cls.py --enable-checkpointing True --explainer-model-name resnet --explainee-model-name resnet --mask-loss-mul 50 --train-model-by-target-gt-class False --n-epochs 30 --train-n-label-sample 6 &> nohups_logs/journal/resnet_6000_bs32_ml50_predicted.out &
     """
     params_config = get_params_from_config(config_vit=config["vit"])
 
